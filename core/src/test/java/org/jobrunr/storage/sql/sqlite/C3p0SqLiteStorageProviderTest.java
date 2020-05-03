956584ca09f632159d88672fd010e6644aa5fa5a
Ronald Dehuysser <ronald.dehuysser@codabox.com>
2020-05-03 12:18:06 +0200

    Try to fix build

--- core/src/test/java/org/jobrunr/storage/sql/sqlite/C3p0SqLiteStorageProviderTest.java
+++ core/src/test/java/org/jobrunr/storage/sql/sqlite/C3p0SqLiteStorageProviderTest.java
@@ -4,10 +4,6 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.jobrunr.storage.sql.SqlStorageProviderTest;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 class C3p0SqLiteStorageProviderTest extends SqlStorageProviderTest {
 
@@ -16,31 +12,17 @@ class C3p0SqLiteStorageProviderTest extends SqlStorageProviderTest {
     /**
      * Strange bug when combining c3p0 and Sqlite where although drop table succeeds, the creation of table fails because it is still there.
      */
-    @Override
     protected void cleanupDatabase(DataSource dataSource) {
-        boolean stillHasTables = true;
-        while (stillHasTables) {
+        try {
             super.cleanupDatabase(dataSource);
-
-            try (Connection connection = dataSource.getConnection()) {
-                final Statement statement = connection.createStatement();
-                final ResultSet resultSet = statement.executeQuery("SELECT count(*) FROM sqlite_master WHERE type='table';");
-                if (resultSet.next()) {
-                    int count = resultSet.getInt(1);
-                    if (count > 1) {
-                        System.err.println(String.format("Still found %d tables.", count));
-                    } else {
-                        System.err.println("No tables found - finally");
-                        stillHasTables = false;
-                    }
-                }
-            } catch (SQLException exception) {
-                throw new RuntimeException(exception);
-            }
+            Thread.sleep(20);
+            super.cleanupDatabase(dataSource);
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
-    @Override
     protected DataSource getDataSource() {
         if (dataSource == null) {
             dataSource = new ComboPooledDataSource();
