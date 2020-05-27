3a352ed95b1eeaa256c7bc4c786fc5dc788e5b6d
Ronald Dehuysser <ronald.dehuysser@codabox.com>
2020-05-27 10:44:10 +0200

    research build

--- tests/e2e-jdk/src/test/java/org/jobrunr/tests/fromhost/BuildAndTestContainer.java
+++ tests/e2e-jdk/src/test/java/org/jobrunr/tests/fromhost/BuildAndTestContainer.java
@@ -5,7 +5,11 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.utility.MountableFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class BuildAndTestContainer extends GenericContainer<BuildAndTestContainer> {
 
@@ -17,10 +21,27 @@ public class BuildAndTestContainer extends GenericContainer<BuildAndTestContaine
                                 .workDir("/app/jobrunr")
                                 .env("JDK_TEST", "true")
                 ));
+        final Path gradleDistPath = Paths.get(System.getProperty("user.home"), ".gradle", "wrapper", "dists");
+        listFiles(gradleDistPath);
         this
-                .withFileSystemBind(Paths.get(System.getProperty("user.home"), ".gradle", "wrapper", "dists").toString(), "/root/.gradle/wrapper/dists")
+                .withFileSystemBind(gradleDistPath.toString(), "/root/.gradle/wrapper/dists")
                 .withCopyFileToContainer(MountableFile.forHostPath(Paths.get(".")), "/app/jobrunr")
                 .withCommand("./gradlew", "build")
                 .waitingFor(Wait.forLogMessage(".*BUILD SUCCESSFUL.*", 1));
+    }
+
+    private void listFiles(Path gradleDistPath) {
+        System.out.println("============================================================");
+        System.out.println("====================   Files     ===========================");
+        System.out.println("============================================================");
+        System.out.println("Path: " + gradleDistPath.toString());
+        try (Stream<Path> stream = Files.walk(gradleDistPath, 2)) {
+            stream
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
