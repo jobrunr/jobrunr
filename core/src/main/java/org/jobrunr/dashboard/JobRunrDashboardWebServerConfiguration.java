4e445911ae39546dd4c98ddf6ba299d82aa989bc
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-08-21 11:27:52 +0200

    Added more configuration options

--- core/src/main/java/org/jobrunr/dashboard/JobRunrDashboardWebServerConfiguration.java
+++ core/src/main/java/org/jobrunr/dashboard/JobRunrDashboardWebServerConfiguration.java
@@ -1,16 +1,30 @@
 package org.jobrunr.dashboard;
 
+/**
+ * This class allows to configure the JobRunrDashboard
+ */
 public class JobRunrDashboardWebServerConfiguration {
     int port = 8000;
 
-    JobRunrDashboardWebServerConfiguration() {
+    private JobRunrDashboardWebServerConfiguration() {
 
     }
 
+    /**
+     * This returns the default configuration with the JobRunrDashboard running on port 8000
+     *
+     * @return the default JobRunrDashboard configuration
+     */
     public static JobRunrDashboardWebServerConfiguration usingStandardConfiguration() {
         return new JobRunrDashboardWebServerConfiguration();
     }
 
+    /**
+     * Specifies the port on which the JobRunrDashboard will run
+     *
+     * @param port the port on which the JobRunrDashboard will run
+     * @return the same configuration instance which provides a fluent api
+     */
     public JobRunrDashboardWebServerConfiguration andPort(int port) {
         this.port = port;
         return this;
--- core/src/main/java/org/jobrunr/server/BackgroundJobServerConfiguration.java
+++ core/src/main/java/org/jobrunr/server/BackgroundJobServerConfiguration.java
@@ -4,24 +4,44 @@ import org.jobrunr.server.configuration.BackgroundJobServerWorkerPolicy;
 import org.jobrunr.server.configuration.DefaultBackgroundJobServerWorkerPolicy;
 import org.jobrunr.server.configuration.FixedSizeBackgroundJobServerWorkerPolicy;
 
+/**
+ * This class allows to configure the BackgroundJobServer
+ */
 public class BackgroundJobServerConfiguration {
 
     int pollIntervalInSeconds = 15;
     BackgroundJobServerWorkerPolicy backgroundJobServerWorkerPolicy = new DefaultBackgroundJobServerWorkerPolicy();
 
-    BackgroundJobServerConfiguration() {
+    private BackgroundJobServerConfiguration() {
 
     }
 
+    /**
+     * This returns the default configuration with the BackgroundJobServer with a poll interval of 15 seconds and a worker count based on the CPU
+     *
+     * @return the default JobRunrDashboard configuration
+     */
     public static BackgroundJobServerConfiguration usingStandardConfiguration() {
         return new BackgroundJobServerConfiguration();
     }
 
+    /**
+     * Allows to set the pollIntervalInSeconds for the BackgroundJobServer
+     *
+     * @param pollIntervalInSeconds the pollIntervalInSeconds
+     * @return the same configuration instance which provides a fluent api
+     */
     public BackgroundJobServerConfiguration andPollIntervalInSeconds(int pollIntervalInSeconds) {
         this.pollIntervalInSeconds = pollIntervalInSeconds;
         return this;
     }
 
+    /**
+     * Allows to set the workerCount for the BackgroundJobServer which defines the maximum number of jobs that will be run in parallel
+     *
+     * @param workerCount the workerCount for the BackgroundJobServer
+     * @return the same configuration instance which provides a fluent api
+     */
     public BackgroundJobServerConfiguration andWorkerCount(int workerCount) {
         this.backgroundJobServerWorkerPolicy = new FixedSizeBackgroundJobServerWorkerPolicy(workerCount);
         return this;
