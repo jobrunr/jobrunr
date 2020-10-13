a5bf3dc4bcefd45f04b5b817fc826c54495565a7
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-10-13 23:26:41 +0200

    Improvements related to #51.

--- jobrunr-spring-boot-starter/src/main/java/org/jobrunr/autoconfigure/storage/JobRunrSqlStorageAutoConfiguration.java
+++ jobrunr-spring-boot-starter/src/main/java/org/jobrunr/autoconfigure/storage/JobRunrSqlStorageAutoConfiguration.java
@@ -4,8 +4,11 @@ import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.storage.StorageProvider;
 import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider;
 import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -14,6 +17,8 @@ import javax.sql.DataSource;
 
 @Configuration
 @ConditionalOnBean(DataSource.class)
+@ConditionalOnSingleCandidate(DataSource.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
 public class JobRunrSqlStorageAutoConfiguration {
 
     @Bean(name = "storageProvider")
