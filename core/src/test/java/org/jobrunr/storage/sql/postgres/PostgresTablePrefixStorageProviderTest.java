c45eddd2e60bbf3095822a245bf6538b0102d172
Neil Deng <neil.deng.g@gmail.com>
2021-06-25 15:23:24 +0800

    add table prefix test case on Postgres and SQLServer

---
+++ core/src/test/java/org/jobrunr/storage/sql/postgres/PostgresTablePrefixStorageProviderTest.java
@@ -0,0 +1,97 @@
+package org.jobrunr.storage.sql.postgres;
+
+import org.assertj.core.api.Condition;
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.storage.StorageProvider;
+import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider;
+import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
+import org.jobrunr.utils.mapper.jackson.JacksonJsonMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jobrunr.utils.resilience.RateLimiter.Builder.rateLimit;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PostgresTablePrefixStorageProviderTest extends AbstractPostgresStorageProviderTest {
+
+    private static PGSimpleDataSource dataSource;
+
+    @BeforeAll
+    void runInitScript() {
+        String packageName = PostgresTablePrefixStorageProviderTest.class.getPackageName();
+        String initScriptPath = packageName.replaceAll("\\.", "/") + "/init_with_schema.sql";
+        JdbcDatabaseDelegate containerDelegate = new JdbcDatabaseDelegate(sqlContainer, "");
+        ScriptUtils.runInitScript(containerDelegate, initScriptPath);
+    }
+
+    @Override
+    protected StorageProvider getStorageProvider() {
+        final StorageProvider storageProvider = SqlStorageProviderFactory.using(getDataSource(), "SOME_SCHEMA.SOME_PREFIX_", DefaultSqlStorageProvider.DatabaseOptions.CREATE);
+        storageProvider.setJobMapper(new JobMapper(new JacksonJsonMapper()));
+        Whitebox.setInternalState(storageProvider, "changeListenerNotificationRateLimit", rateLimit().withoutLimits());
+        return storageProvider;
+    }
+
+    protected void cleanupDatabase(DataSource dataSource) {
+        drop(dataSource, "view SOME_SCHEMA.SOME_PREFIX_jobrunr_jobs_stats");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_recurring_jobs");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_job_counters");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_jobs");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_backgroundjobservers");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_migrations");
+        drop(dataSource, "table SOME_SCHEMA.SOME_PREFIX_jobrunr_metadata");
+    }
+
+    @Override
+    protected DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new PGSimpleDataSource();
+            dataSource.setURL(sqlContainer.getJdbcUrl());
+            dataSource.setUser(sqlContainer.getUsername());
+            dataSource.setPassword(sqlContainer.getPassword());
+        }
+        return dataSource;
+    }
+
+    @AfterEach
+    void checkTablesCreatedWithCorrectPrefix() throws SQLException {
+        try (final Connection connection = dataSource.getConnection(); final Statement statement = connection.createStatement()) {
+            try (ResultSet rs = statement.executeQuery("SELECT * FROM INFORMATION_SCHEMA.TABLES")) {
+                List<String> allTables = new ArrayList<>();
+                while (rs.next()) {
+                    String tableCatalog = rs.getString(1);
+                    String tableSchema = rs.getString(2);
+                    String tableName = rs.getString(3);
+
+                    allTables.add(tableSchema + "." + tableName);
+                }
+
+                assertThat(allTables).areAtLeast(5, new Condition<>(name -> name.startsWith("SOME_SCHEMA.SOME_PREFIX_JOBRUNR_"), ""));
+            }
+
+            try (ResultSet rs = statement.executeQuery("SELECT indexname FROM pg_indexes")) {
+                List<String> allIndices = new ArrayList<>();
+                while (rs.next()) {
+                    String indexName = rs.getString("indexname");
+                    allIndices.add(indexName);
+                }
+
+                assertThat(allIndices).areAtLeast(8, new Condition<>(name -> name.toUpperCase().startsWith("SOME_PREFIX_JOBRUNR_"), ""));
+            }
+        }
+    }
+}
