dd8dbce49300f5a2eba2b9ea17be226ec78417da
Pei-Tang Huang <tangtheone@gmail.com>
2021-07-14 22:08:41 +0800

    Provides default method on interfaces with more than one method. So that people can implementing only the required parts of the interface.

--- core/src/main/java/org/jobrunr/jobs/filters/JobServerFilter.java
+++ core/src/main/java/org/jobrunr/jobs/filters/JobServerFilter.java
@@ -8,8 +8,8 @@ import org.jobrunr.jobs.Job;
  */
 public interface JobServerFilter extends JobFilter {
 
-    void onProcessing(Job job);
+    default void onProcessing(Job job) {}
 
-    void onProcessed(Job job);
+    default void onProcessed(Job job) {}
 
 }
