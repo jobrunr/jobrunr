6b3682577e19c872affd9c5d7a6dab1c137c16f6
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-07-31 15:25:34 +0200

    Fix #184

--- core/src/test/java/org/jobrunr/scheduling/JobSchedulerTest.java
+++ core/src/test/java/org/jobrunr/scheduling/JobSchedulerTest.java
@@ -3,7 +3,10 @@ package org.jobrunr.scheduling;
 import org.jobrunr.jobs.AbstractJob;
 import org.jobrunr.jobs.Job;
 import org.jobrunr.jobs.RecurringJob;
+import org.jobrunr.jobs.filters.ApplyStateFilter;
+import org.jobrunr.jobs.filters.ElectStateFilter;
 import org.jobrunr.jobs.filters.JobClientFilter;
+import org.jobrunr.jobs.states.JobState;
 import org.jobrunr.scheduling.cron.Cron;
 import org.jobrunr.storage.StorageProvider;
 import org.jobrunr.stubs.TestService;
@@ -13,13 +16,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.jobrunr.jobs.JobTestBuilder.anEnqueuedJob;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,17 +42,30 @@ class JobSchedulerTest {
         testService = new TestService();
 
         jobClientLogFilter = new JobClientLogFilter();
-        jobScheduler = new JobScheduler(storageProvider, Arrays.asList(jobClientLogFilter));
+        jobScheduler = new JobScheduler(storageProvider, List.of(jobClientLogFilter));
     }
 
     @Test
-    void onJobCreatingAndCreatedAreCalled() {
+    void onSaveJobCreatingAndCreatedAreCalled() {
         when(storageProvider.save(any(Job.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
         jobScheduler.enqueue(() -> testService.doWork());
 
         assertThat(jobClientLogFilter.onCreating).isTrue();
         assertThat(jobClientLogFilter.onCreated).isTrue();
+    }
+
+    @Test
+    void onDeleteJobStateElectionAndStateAppliedAreCalled() {
+        final Job enqueuedJob = anEnqueuedJob().build();
+        when(storageProvider.save(any(Job.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(storageProvider.getJobById(enqueuedJob.getId())).thenReturn(enqueuedJob);
+
+        jobScheduler.delete(enqueuedJob.getId());
+
+        assertThat(jobClientLogFilter.onStateElection).isTrue();
+        assertThat(jobClientLogFilter.onStateApplied).isTrue();
+        verify(storageProvider).save(any(Job.class));
     }
 
     @Test
@@ -71,10 +89,12 @@ class JobSchedulerTest {
         assertThat(jobClientLogFilter.onCreated).isTrue();
     }
 
-    private static class JobClientLogFilter implements JobClientFilter {
+    private static class JobClientLogFilter implements JobClientFilter, ElectStateFilter, ApplyStateFilter {
 
         private boolean onCreating;
         private boolean onCreated;
+        private boolean onStateElection;
+        private boolean onStateApplied;
 
         @Override
         public void onCreating(AbstractJob job) {
@@ -84,6 +104,16 @@ class JobSchedulerTest {
         @Override
         public void onCreated(AbstractJob job) {
             onCreated = true;
+        }
+
+        @Override
+        public void onStateElection(Job job, JobState newState) {
+            onStateElection = true;
+        }
+
+        @Override
+        public void onStateApplied(Job job, JobState oldState, JobState newState) {
+            onStateApplied = true;
         }
     }
 
