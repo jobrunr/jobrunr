c9b376e38ddb0f7ced6b5c16a9ad2178a17a2425
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-09-07 20:55:34 +0200

    Add extra test to clearing JobRunrContext [SKIP CI]

--- core/src/test/java/org/jobrunr/server/runner/AbstractBackgroundJobRunnerTest.java
+++ core/src/test/java/org/jobrunr/server/runner/AbstractBackgroundJobRunnerTest.java
@@ -1,11 +1,16 @@
 package org.jobrunr.server.runner;
 
 import org.jobrunr.jobs.Job;
+import org.jobrunr.server.runner.AbstractBackgroundJobRunner.BackgroundJobWorker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.jobrunr.jobs.JobTestBuilder.aJobInProgress;
 import static org.mockito.Mockito.doAnswer;
@@ -15,7 +20,7 @@ import static org.mockito.Mockito.doThrow;
 class AbstractBackgroundJobRunnerTest {
 
     @Mock
-    private AbstractBackgroundJobRunner.BackgroundJobWorker worker;
+    private BackgroundJobWorker worker;
 
     @Test
     void ifCodeThrowsInterruptedException_AnInterruptExceptionIsThrown() throws Exception {
@@ -38,7 +43,31 @@ class AbstractBackgroundJobRunnerTest {
         assertThatThrownBy(() -> backgroundJobRunner.run(job)).isInstanceOf(InterruptedException.class);
     }
 
+    @Test
+    void invokeJobMethodUpdatesJobContextThreadLocal() throws Exception {
+        final Job job = aJobInProgress().withJobDetails(this::testJobContext).build();
+
+        final AbstractBackgroundJobRunner backgroundJobRunner = getJobRunner(BackgroundJobWorker::new);
+
+        backgroundJobRunner.run(job);
+        assertThat(ThreadLocalJobContext.getJobContext()).isNull();
+    }
+
+    @Test
+    void invokeJobMethodAlwaysResetsJobContextThreadLocal() {
+        final Job job = aJobInProgress().withJobDetails(this::throwingJobContext).build();
+
+        final AbstractBackgroundJobRunner backgroundJobRunner = getJobRunner(BackgroundJobWorker::new);
+
+        assertThatCode(() -> backgroundJobRunner.run(job)).isInstanceOf(Exception.class);
+        assertThat(ThreadLocalJobContext.getJobContext()).isNull();
+    }
+
     private AbstractBackgroundJobRunner getJobRunner() {
+        return getJobRunner(job -> worker);
+    }
+
+    private AbstractBackgroundJobRunner getJobRunner(Function<Job, BackgroundJobWorker> worker) {
         return new AbstractBackgroundJobRunner() {
             @Override
             public boolean supports(Job job) {
@@ -47,9 +76,18 @@ class AbstractBackgroundJobRunnerTest {
 
             @Override
             protected BackgroundJobWorker getBackgroundJobWorker(Job job) {
-                return worker;
+                return worker.apply(job);
             }
         };
+    }
+
+    public void testJobContext() {
+        assertThat(ThreadLocalJobContext.getJobContext()).isNotNull();
+    }
+
+    public void throwingJobContext() {
+        assertThat(ThreadLocalJobContext.getJobContext()).isNotNull();
+        throw new RuntimeException("Boem");
     }
 
 }
