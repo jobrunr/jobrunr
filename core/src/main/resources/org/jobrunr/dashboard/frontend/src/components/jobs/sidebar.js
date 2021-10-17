1437c9d3cd91f3c2b2b6daee98cdaf75d3164dc0
Ted Liang <teddyliang@gmail.com>
2021-10-17 20:16:33 +1100

    Make sidebar collapsable

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/sidebar.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/sidebar.js
@@ -1,41 +1,24 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Chip from '@material-ui/core/Chip';
-import {makeStyles} from '@material-ui/core/styles';
 import {Schedule} from "@material-ui/icons";
 import {AlertCircleOutline, Check, Cogs, Delete, TimerSand} from "mdi-material-ui";
 import statsState from "../../StatsStateContext";
 
-const drawerWidth = 320;
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    content: {
-        width: '100%',
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    nested: {
-        paddingLeft: theme.spacing(4),
-    },
-    toolbar: theme.mixins.toolbar,
-}));
+const categories = [
+    {name: "scheduled", state: "SCHEDULED", label: "Scheduled", icon: <Schedule/>},
+    {name: "enqueued", state: "ENQUEUED", label: "Enqueued", icon: <TimerSand/>},
+    {name: "processing", state: "PROCESSING", label: "Processing", icon: <Cogs/>},
+    {name: "succeeded", state: "SUCCEEDED", label: "Succeeded", icon: <Check/>},
+    {name: "failed", state: "FAILED", label: "Failed", icon: <AlertCircleOutline/>},
+    {name: "deleted", state: "DELETED", label: "Deleted", icon: <Delete/>},
+];
 
 const Sidebar = () => {
-    const classes = useStyles();
-
     const [stats, setStats] = React.useState(statsState.getStats());
     React.useEffect(() => {
         statsState.addListener(setStats);
@@ -43,50 +26,18 @@ const Sidebar = () => {
     }, [])
 
     return (
-        <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-                paper: classes.drawerPaper,
-            }}
-        >
-            <div className={classes.toolbar}/>
-            <List>
-                <List component="div" disablePadding>
-                    <ListItem id="scheduled-menu-btn" button key="Scheduled" component={Link}
-                              to="/dashboard/jobs?state=SCHEDULED">
-                        <ListItemIcon><Schedule/></ListItemIcon>
-                        <ListItemText primary="Scheduled"/><Chip label={stats.scheduled}/>
+        <List>
+            <List component="div" disablePadding>
+                {categories.map(({name, state, label, icon}) => (
+                    <ListItem id={`${name}-menu-btn`} button key={label} title={label}
+                              component={Link} to={`/dashboard/jobs?state=${state}`}>
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={label}/>
+                        <Chip label={stats[name]}/>
                     </ListItem>
-                    <ListItem id="enqueued-menu-btn" button key="Enqueued"
-                              component={Link} to="/dashboard/jobs?state=ENQUEUED">
-                        <ListItemIcon><TimerSand/></ListItemIcon>
-                        <ListItemText primary="Enqueued"/>
-                        <Chip label={stats.enqueued}/>
-                    </ListItem>
-                    <ListItem id="processing-menu-btn" button key="Processing"
-                              component={Link} to="/dashboard/jobs?state=PROCESSING">
-                        <ListItemIcon><Cogs/></ListItemIcon>
-                        <ListItemText primary="Processing"/><Chip label={stats.processing}/>
-                    </ListItem>
-                    <ListItem id="succeeded-menu-btn" button key="Succeeded"
-                              component={Link} to="/dashboard/jobs?state=SUCCEEDED">
-                        <ListItemIcon><Check/></ListItemIcon>
-                        <ListItemText primary="Succeeded"/><Chip label={stats.succeeded}/>
-                    </ListItem>
-                    <ListItem id="failed-menu-btn" button key="Failed" component={Link}
-                              to="/dashboard/jobs?state=FAILED">
-                        <ListItemIcon><AlertCircleOutline/></ListItemIcon>
-                        <ListItemText primary="Failed"/><Chip label={stats.failed}/>
-                    </ListItem>
-                    <ListItem id="deleted-menu-btn" button key="Deleted" component={Link}
-                              to="/dashboard/jobs?state=DELETED">
-                        <ListItemIcon><Delete/></ListItemIcon>
-                        <ListItemText primary="Deleted"/><Chip label={stats.deleted}/>
-                    </ListItem>
-                </List>
+                ))}
             </List>
-        </Drawer>
+        </List>
     );
 };
 
--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/utils/with-sidebar.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/utils/with-sidebar.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import Drawer from "@material-ui/core/Drawer";
+import {IconButton} from "@material-ui/core";
+import {ChevronLeft, ChevronRight} from "mdi-material-ui";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
-    }
+    },
+    toolbar: theme.mixins.toolbar,
+    drawer: {
+        width: 260,
+    },
+    drawerCollapsed: {
+        width: theme.spacing(7) + 1,
+        overflowX: 'hidden',
+    },
+    toggle: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingRight: theme.spacing(0.5),
+    },
 }));
 
 const WithSidebar = (Sidebar, Component) => {
     const classes = useStyles();
-    return (props) => {
-        return (
-            <div className={classes.root}>
+    const [collapsed, setCollapsed] = React.useState(false);
+    const className = classes[collapsed ? 'drawerCollapsed' : 'drawer'];
+    return (props) => (
+        <div className={classes.root}>
+            <Drawer variant="permanent" className={className} classes={{paper: className}}>
+                <div className={classes.toolbar}/>
                 <Sidebar {...props} />
-                <Component {...props} />
-            </div>
-        );
-    };
+                <div className={classes.toggle}>
+                    <IconButton onClick={() => setCollapsed(!collapsed)}>
+                        {collapsed ? <ChevronRight/> : <ChevronLeft/>}
+                    </IconButton>
+                </div>
+            </Drawer>
+            <Component {...props} />
+        </div>
+    );
 }
 
 export default WithSidebar;
