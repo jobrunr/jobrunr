f8a4d14d045df236b028ed636af0ea37e79aa2a9
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-10-22 22:40:15 +0200

    Update dependencies

--- core/src/test/java/org/jobrunr/utils/metadata/VersionRetrieverTest.java
+++ core/src/test/java/org/jobrunr/utils/metadata/VersionRetrieverTest.java
@@ -25,7 +25,7 @@ class VersionRetrieverTest {
 
     @Test
     void jacksonVersion() {
-        assertThat(VersionRetriever.getVersion(ObjectMapper.class)).isEqualTo("2.12.4");
+        assertThat(VersionRetriever.getVersion(ObjectMapper.class)).isEqualTo("2.13.0");
     }
 
 }
--- framework-support/jobrunr-quarkus-extension/runtime/src/test/java/org/jobrunr/quarkus/autoconfigure/metrics/JobRunrMetricsStarterTest.java
+++ framework-support/jobrunr-quarkus-extension/runtime/src/test/java/org/jobrunr/quarkus/autoconfigure/metrics/JobRunrMetricsStarterTest.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.enterprise.inject.Instance;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
