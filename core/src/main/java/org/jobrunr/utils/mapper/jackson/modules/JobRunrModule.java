56b371cff7f04e554b7ea13b15ed12eb3432f59a
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2022-03-07 23:04:35 +0100

    Fix #375

--- core/src/main/java/org/jobrunr/utils/mapper/jackson/modules/JobRunrModule.java
+++ core/src/main/java/org/jobrunr/utils/mapper/jackson/modules/JobRunrModule.java
@@ -1,7 +1,14 @@
 package org.jobrunr.utils.mapper.jackson.modules;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.ValueNode;
 import org.jobrunr.jobs.JobParameter;
+
+import java.io.IOException;
 
 public class JobRunrModule extends SimpleModule {
 
