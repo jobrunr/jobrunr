a95e3c22e853699d8ddbb8d1ba4de0797f50af80
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2022-03-13 10:49:11 +0100

    More build fixing

--- tests/e2e-mariadb-jackson/src/main/java/org/jobrunr/tests/e2e/Main.java
+++ tests/e2e-mariadb-jackson/src/main/java/org/jobrunr/tests/e2e/Main.java
@@ -18,7 +18,7 @@ public class Main extends AbstractSqlMain {
     @Override
     protected DataSource createDataSource(String jdbcUrl, String userName, String password) throws SQLException {
         MariaDbPoolDataSource dataSource = new MariaDbPoolDataSource();
-        dataSource.setUrl(jdbcUrl + "?rewriteBatchedStatements=true&pool=true");
+        dataSource.setUrl(jdbcUrl + "?rewriteBatchedStatements=true&pool=true&useBulkStmts=false");
         dataSource.setUser(userName);
         dataSource.setPassword(password);
         return dataSource;
