c9c8459b72d95f6709fea02efd7591614c7ce7ee
Razvan Prichici <razvanpaulp@gmail.com>
2022-05-28 14:14:39 +0300

    Add backgroundJobServer restart option - fix 473

--- core/src/main/java/org/jobrunr/server/BackgroundJobServer.java
+++ core/src/main/java/org/jobrunr/server/BackgroundJobServer.java
@@ -128,8 +128,17 @@ public class BackgroundJobServer implements BackgroundJobServerMBean {
         }
     }
 
+    public void restart() {
+        forceStop();
+        start();
+    }
+
     public void stop() {
         if (isStopped()) return;
+        forceStop();
+    }
+
+    private void forceStop() {
         try (BackgroundJobServerLifecycleLock ignored = lifecycleLock.lock()) {
             LOGGER.info("BackgroundJobServer and BackgroundJobPerformers - stopping (waiting for all jobs to complete - max 10 seconds)");
             isMaster = null;
