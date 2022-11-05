cbc127a46813ca208872455c5e24d15c171ee358
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2022-11-05 14:47:59 +0100

    Fix #598

--- core/src/main/java/org/jobrunr/storage/sql/common/db/SqlResultSet.java
+++ core/src/main/java/org/jobrunr/storage/sql/common/db/SqlResultSet.java
@@ -1,35 +1,28 @@
 package org.jobrunr.storage.sql.common.db;
 
-import java.math.BigDecimal;
+import org.jobrunr.storage.StorageException;
+
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.*;
+import java.time.ZoneOffset;
+import java.util.Calendar;
+import java.util.List;
+import java.util.UUID;
 
+import static java.util.TimeZone.getTimeZone;
 import static org.jobrunr.utils.reflection.ReflectionUtils.autobox;
 
 public class SqlResultSet {
 
-    private final Set<String> columns;
-    private final Map<String, Object> values;
+    private final List<String> columns;
+    private final ResultSet rs;
 
-    public SqlResultSet(ResultSet rs) throws SQLException {
-        this.columns = getColumns(rs);
-        this.values = initValues(rs, columns);
-    }
-
-    private Map<String, Object> initValues(ResultSet rs, Set<String> columns) throws SQLException {
-        Map<String, Object> result = new HashMap<>();
-        for (String columnName : columns) {
-            result.put(columnName, rs.getObject(columnName));
-        }
-        return result;
-    }
-
-    private Object val(String name) {
-        return values.get(name.toLowerCase());
+    public SqlResultSet(List<String> columns, ResultSet rs) {
+        this.columns = columns;
+        this.rs = rs;
     }
 
     public boolean asBoolean(String name) {
@@ -53,31 +46,30 @@ public class SqlResultSet {
     }
 
     public Instant asInstant(String name) {
-        return autobox(val(name), Instant.class);
+        return autobox(valAsUTCTimestamp(name), Instant.class);
     }
 
     public Duration asDuration(String name) {
         return autobox(val(name), Duration.class);
     }
 
-    public float asFloat(String name) {
-        return autobox(val(name), Float.class);
-    }
-
     public double asDouble(String name) {
         return autobox(val(name), Double.class);
     }
 
-    public BigDecimal asBigDecimal(String name) {
-        return autobox(val(name), BigDecimal.class);
+    private Object val(String name) {
+        try {
+            return rs.getObject(columns.indexOf(name.toLowerCase()));
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
-    private Set<String> getColumns(ResultSet resultSet) throws SQLException {
-        Set<String> result = new HashSet<>();
-        final ResultSetMetaData metaData = resultSet.getMetaData();
-        for (int i = 1; i <= metaData.getColumnCount(); i++) {
-            result.add(metaData.getColumnName(i).toLowerCase());
+    private Timestamp valAsUTCTimestamp(String name) {
+        try {
+            return rs.getTimestamp(columns.indexOf(name.toLowerCase()), Calendar.getInstance(getTimeZone(ZoneOffset.UTC)));
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
-        return result;
     }
 }
--- core/src/test/java/org/jobrunr/dashboard/FrontEndDevelopment.java
+++ core/src/test/java/org/jobrunr/dashboard/FrontEndDevelopment.java
@@ -1,5 +1,7 @@
 package org.jobrunr.dashboard;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.jobrunr.SevereJobRunrException;
 import org.jobrunr.configuration.JobRunr;
 import org.jobrunr.jobs.context.JobContext;
@@ -11,19 +13,19 @@ import org.jobrunr.server.dashboard.CpuAllocationIrregularityNotification;
 import org.jobrunr.server.dashboard.DashboardNotificationManager;
 import org.jobrunr.storage.InMemoryStorageProvider;
 import org.jobrunr.storage.StorageProvider;
-import org.jobrunr.storage.StubDataProvider;
+import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
 import org.jobrunr.stubs.TestService;
 import org.jobrunr.utils.diagnostics.DiagnosticsBuilder;
 import org.jobrunr.utils.mapper.jackson.JacksonJsonMapper;
+import org.postgresql.ds.PGSimpleDataSource;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.MINUTES;
 import static org.jobrunr.jobs.JobDetailsTestBuilder.*;
 import static org.jobrunr.jobs.JobTestBuilder.aJob;
@@ -34,14 +36,13 @@ import static org.jobrunr.utils.diagnostics.DiagnosticsBuilder.diagnostics;
  */
 public class FrontEndDevelopment {
 
-    public static void main(String[] args) throws InterruptedException {
-        StorageProvider storageProvider = new InMemoryStorageProvider();
-        storageProvider.setJobMapper(new JobMapper(new JacksonJsonMapper()));
+    public static void main(String[] args) throws Exception {
+        StorageProvider storageProvider = inMemoryStorageProvider();
 
-        StubDataProvider.using(storageProvider)
+        //StubDataProvider.using(storageProvider)
                 //.addALotOfEnqueuedJobsThatTakeSomeTime()
                 //.addALotOfEnqueuedJobsThatTakeSomeTime()
-                .addSomeRecurringJobs();
+                //.addSomeRecurringJobs();
 
         storageProvider.save(aJob().withJobDetails(classThatDoesNotExistJobDetails()).withState(new ScheduledState(Instant.now().plus(2, MINUTES))).build());
         storageProvider.save(aJob().withJobDetails(methodThatDoesNotExistJobDetails()).withState(new ScheduledState(Instant.now().plus(2, MINUTES))).build());
@@ -94,5 +95,81 @@ public class FrontEndDevelopment {
         public DiagnosticsBuilder getDiagnosticsInfo() {
             return diagnostics().withTitle("Title").withLine("Text").withException(new RuntimeException());
         }
+    }
+
+    private static StorageProvider inMemoryStorageProvider() throws SQLException {
+        StorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(new JobMapper(new JacksonJsonMapper()));
+        return storageProvider;
+    }
+
+    private static StorageProvider db2StorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:db2://127.0.0.1:53759/test");
+        config.setUsername("db2inst1");
+        config.setPassword("foobar1234");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider h2StorageProvider() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:h2:/tmp/test-frontend");
+        config.setUsername("sa");
+        config.setPassword("sa");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider mariaDBStorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/mysql?rewriteBatchedStatements=true&useBulkStmts=false");
+        config.setUsername("root");
+        config.setPassword("mysql");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider mysqlStorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://127.0.0.1:50516/test?rewriteBatchedStatements=true&useSSL=false");
+        config.setUsername("test");
+        config.setPassword("test");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider oracleStorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:54076/xepdb1");
+        config.setUsername("test");
+        config.setPassword("test");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider postgresStorageProvider() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL("jdbc:postgresql://127.0.0.1:5432/postgres");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        dataSource.setProperty("socketTimeout", "10");
+        return toStorageProvider(dataSource);
+    }
+
+    private static StorageProvider sqliteStorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:/tmp/jobrunr-frontend.db");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider sqlServerStorageProvider() throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlserver://localhost:1433;databaseName=tempdb;encrypt=true;trustServerCertificate=true;");
+        config.setUsername("sa");
+        config.setPassword("yourStrong(!)Password");
+        return toStorageProvider(new HikariDataSource(config));
+    }
+
+    private static StorageProvider toStorageProvider(DataSource dataSource) {
+        StorageProvider storageProvider = SqlStorageProviderFactory.using(dataSource);
+        storageProvider.setJobMapper(new JobMapper(new JacksonJsonMapper()));
+        return storageProvider;
     }
 }
