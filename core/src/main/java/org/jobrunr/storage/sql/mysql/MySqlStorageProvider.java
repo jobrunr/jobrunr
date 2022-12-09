367947d94822e7f8a061eb93bc524810c99fb8c0
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2022-12-09 13:34:35 +0100

    Improvements for deadlock in MariaDB and MySQL (see #635)

--- core/src/main/java/org/jobrunr/storage/sql/mysql/MySqlStorageProvider.java
+++ core/src/main/java/org/jobrunr/storage/sql/mysql/MySqlStorageProvider.java
@@ -1,5 +1,6 @@
 package org.jobrunr.storage.sql.mysql;
 
+import org.jobrunr.storage.BackgroundJobServerStatus;
 import org.jobrunr.storage.StorageProviderUtils.DatabaseOptions;
 import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider;
 import org.jobrunr.storage.sql.common.db.dialect.AnsiDialect;
@@ -27,8 +28,27 @@ public class MySqlStorageProvider extends DefaultSqlStorageProvider {
     }
 
     @Override
+    public void announceBackgroundJobServer(BackgroundJobServerStatus serverStatus) {
+        // why https://github.com/jobrunr/jobrunr/issues/635
+        Exceptions.retryOnException(() -> super.announceBackgroundJobServer(serverStatus), 5);
+
+    }
+
+    @Override
     public int removeTimedOutBackgroundJobServers(Instant heartbeatOlderThan) {
         //why: https://github.com/jobrunr/jobrunr/issues/275
         return Exceptions.retryOnException(() -> super.removeTimedOutBackgroundJobServers(heartbeatOlderThan), 5);
+    }
+
+    @Override
+    public boolean signalBackgroundJobServerAlive(BackgroundJobServerStatus serverStatus) {
+        //why: https://github.com/jobrunr/jobrunr/issues/635
+        return Exceptions.retryOnException(() -> super.signalBackgroundJobServerAlive(serverStatus), 5);
+    }
+
+    @Override
+    public void signalBackgroundJobServerStopped(BackgroundJobServerStatus serverStatus) {
+        // why https://github.com/jobrunr/jobrunr/issues/635
+        Exceptions.retryOnException(() -> super.signalBackgroundJobServerStopped(serverStatus), 5);
     }
 }
