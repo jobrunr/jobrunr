4ff97cd83e8991e48161cbeb54794b9d98ccad13
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-01-30 11:40:26 +0100

    Fix #665

--- core/src/main/resources/org/jobrunr/storage/sql/sqlserver/migrations/v011__change_sqlserver_text_to_varchar.sql
+++ core/src/main/resources/org/jobrunr/storage/sql/sqlserver/migrations/v011__change_sqlserver_text_to_varchar.sql
@@ -2,5 +2,7 @@ ALTER TABLE jobrunr_jobs
     ALTER COLUMN jobAsJson NVARCHAR(MAX);
 ALTER TABLE jobrunr_jobs
     ALTER COLUMN jobSignature NVARCHAR(512);
+ALTER TABLE jobrunr_recurring_jobs
+    ALTER COLUMN jobAsJson NVARCHAR(MAX);
 ALTER TABLE jobrunr_metadata
     ALTER COLUMN value NVARCHAR(MAX);
