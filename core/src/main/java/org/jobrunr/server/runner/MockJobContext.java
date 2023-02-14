019e577ad69f66b70cd487f8cee91a55870e157e
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-02-14 20:52:24 +0100

    Fix #671

--- core/src/main/java/org/jobrunr/server/runner/MockJobContext.java
+++ core/src/main/java/org/jobrunr/server/runner/MockJobContext.java
@@ -1,5 +1,6 @@
 package org.jobrunr.server.runner;
 
+import org.jobrunr.jobs.Job;
 import org.jobrunr.jobs.context.JobContext;
 
 /**
@@ -9,6 +10,19 @@ public class MockJobContext {
 
     private MockJobContext() {}
 
+    public static void setUpJobContextForJob(Job job) {
+        MockJobContext.setJobContext(new RunnerJobContext(job));
+    }
+
+    public static void setUpJobContext(JobContext jobContext) {
+        ThreadLocalJobContext.setJobContext(jobContext);
+    }
+
+    /**
+     * @param jobContext the JobContext to setup
+     * @deprecated use {@link MockJobContext#setUpJobContext(JobContext)}
+     */
+    @Deprecated
     public static void setJobContext(JobContext jobContext) {
         ThreadLocalJobContext.setJobContext(jobContext);
     }
