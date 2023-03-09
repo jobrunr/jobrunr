487cd04b4ad34b008cd48fb1899071ca312a1ca0
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-03-09 14:15:31 +0100

    Ad test for #717, fix #317 (again), fix #677

---
+++ core/src/test/java/org/jobrunr/storage/sql/mysql/HikariMySQLServerPrepStatementsStorageProviderTest.java
@@ -0,0 +1,31 @@
+package org.jobrunr.storage.sql.mysql;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.AfterAll;
+
+import javax.sql.DataSource;
+
+class HikariMySQLServerPrepStatementsStorageProviderTest extends AbstractMySQLStorageProviderTest {
+
+    private static HikariDataSource dataSource;
+
+    @Override
+    protected DataSource getDataSource() {
+        if (dataSource == null) {
+            HikariConfig config = new HikariConfig();
+
+            config.setDriverClassName("com.mysql.jdbc.Driver");
+            config.setJdbcUrl(sqlContainer.getJdbcUrl() + "?useServerPrepStmts=true&rewriteBatchedStatements=true&useSSL=false");
+            config.setUsername(sqlContainer.getUsername());
+            config.setPassword(sqlContainer.getPassword());
+            dataSource = new HikariDataSource(config);
+        }
+        return dataSource;
+    }
+
+    @AfterAll
+    public static void destroyDatasource() {
+        dataSource.close();
+    }
+}
