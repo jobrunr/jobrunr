57fd6e5851ed11a170285311ba2a557b5da0c209
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-06-01 12:57:12 +0200

    Extra fix related to #777

--- core/src/main/java/org/jobrunr/utils/mapper/jsonb/serializer/DurationTypeDeserializer.java
+++ core/src/main/java/org/jobrunr/utils/mapper/jsonb/serializer/DurationTypeDeserializer.java
@@ -1,5 +1,6 @@
 package org.jobrunr.utils.mapper.jsonb.serializer;
 
+import jakarta.json.JsonValue;
 import jakarta.json.bind.serializer.DeserializationContext;
 import jakarta.json.bind.serializer.JsonbDeserializer;
 import jakarta.json.stream.JsonParser;
@@ -12,10 +13,14 @@ public class DurationTypeDeserializer implements JsonbDeserializer<Duration> {
 
     @Override
     public Duration deserialize(JsonParser jsonParser, DeserializationContext deserializationContext, Type type) {
-        final BigDecimal durationAsSecAndNanoSec = jsonParser.getBigDecimal();
-        return Duration.ofSeconds(
-                durationAsSecAndNanoSec.longValue(),
-                durationAsSecAndNanoSec.remainder(BigDecimal.ONE).movePointRight(durationAsSecAndNanoSec.scale()).abs().longValue()
-        );
+        JsonValue value = jsonParser.getValue();
+        if (value != JsonValue.NULL) {
+            final BigDecimal durationAsSecAndNanoSec = jsonParser.getBigDecimal();
+            return Duration.ofSeconds(
+                    durationAsSecAndNanoSec.longValue(),
+                    durationAsSecAndNanoSec.remainder(BigDecimal.ONE).movePointRight(durationAsSecAndNanoSec.scale()).abs().longValue()
+            );
+        }
+        return null;
     }
 }
