8d2511d48567f29b4af94bed7b786fa4a683f818
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-12-29 10:20:27 +0100

    More build fixing.

--- core/src/test/java/org/jobrunr/jobs/filters/JobFilterUtilsTest.java
+++ core/src/test/java/org/jobrunr/jobs/filters/JobFilterUtilsTest.java
@@ -1,42 +1,62 @@
 package org.jobrunr.jobs.filters;
 
 import org.jobrunr.jobs.Job;
+import org.jobrunr.server.BackgroundJobServer;
 import org.jobrunr.server.LogAllStateChangesFilter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.jobrunr.JobRunrAssertions.assertThat;
-import static org.jobrunr.jobs.JobTestBuilder.aJobInProgress;
+import static org.jobrunr.jobs.JobTestBuilder.anEnqueuedJob;
+import static org.jobrunr.server.BackgroundJobServerConfiguration.usingStandardBackgroundJobServerConfiguration;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class JobFilterUtilsTest {
 
     private LogAllStateChangesFilter logAllStateChangesFilter;
     private JobFilterUtils jobFilterUtils;
+
+    @Mock
+    private BackgroundJobServer backgroundJobServer;
 
     @BeforeEach
     void setUpJobFilterUtils() {
         logAllStateChangesFilter = new LogAllStateChangesFilter();
         JobDefaultFilters jobDefaultFilters = new JobDefaultFilters(logAllStateChangesFilter);
         jobFilterUtils = new JobFilterUtils(jobDefaultFilters);
+
+        when(backgroundJobServer.getConfiguration()).thenReturn(usingStandardBackgroundJobServerConfiguration());
     }
 
     @Test
     void ifExecuteJobServerFilterIsTrueOnStateAppliedFilterIsInvoked() {
-        Job aJobInProgress = aJobInProgress().build();
+        // GIVEN
+        Job aJob = anEnqueuedJob().build();
 
-        jobFilterUtils.runOnStateAppliedFilters(List.of(aJobInProgress), true);
+        // WHEN
+        aJob.startProcessingOn(backgroundJobServer);
+        jobFilterUtils.runOnStateAppliedFilters(List.of(aJob), true);
 
-        assertThat(logAllStateChangesFilter.getStateChanges(aJobInProgress)).containsExactly("ENQUEUED->PROCESSING");
+        // THEN
+        assertThat(logAllStateChangesFilter.getStateChanges(aJob)).containsExactly("ENQUEUED->PROCESSING");
     }
 
     @Test
     void ifExecuteJobServerFilterIsFalseOnStateAppliedFilterIsNotInvoked() {
-        Job aJobInProgress = aJobInProgress().build();
+        // GIVEN
+        Job aJob = anEnqueuedJob().build();
 
-        jobFilterUtils.runOnStateAppliedFilters(List.of(aJobInProgress), false);
+        // WHEN
+        aJob.startProcessingOn(backgroundJobServer);
+        jobFilterUtils.runOnStateAppliedFilters(List.of(aJob), false);
 
-        assertThat(logAllStateChangesFilter.getStateChanges(aJobInProgress)).isEmpty();
+        // THEN
+        assertThat(logAllStateChangesFilter.getStateChanges(aJob)).isEmpty();
     }
 }
