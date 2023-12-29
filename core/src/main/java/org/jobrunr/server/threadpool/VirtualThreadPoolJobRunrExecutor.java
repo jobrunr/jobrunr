6ba8939d7cd26927a81b90adc5561f7363e06113
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-12-29 20:13:58 +0100

    Add tests and logging

--- core/src/main/java/org/jobrunr/server/threadpool/VirtualThreadPoolJobRunrExecutor.java
+++ core/src/main/java/org/jobrunr/server/threadpool/VirtualThreadPoolJobRunrExecutor.java
@@ -1,5 +1,8 @@
 package org.jobrunr.server.threadpool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,13 +11,19 @@ import static org.jobrunr.utils.reflection.ReflectionUtils.getMethod;
 
 public class VirtualThreadPoolJobRunrExecutor implements JobRunrExecutor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(VirtualThreadPoolJobRunrExecutor.class);
+
     private final ExecutorService executorService;
     private final int workerCount;
     private boolean started;
 
     public VirtualThreadPoolJobRunrExecutor(int workerCount) {
+        this(workerCount, createVirtualThreadExecutorService());
+    }
+
+    public VirtualThreadPoolJobRunrExecutor(int workerCount, ExecutorService executorService) {
         this.workerCount = workerCount;
-        executorService = createVirtualThreadExecutorService();
+        this.executorService = executorService;
     }
 
     @Override
@@ -25,6 +34,7 @@ public class VirtualThreadPoolJobRunrExecutor implements JobRunrExecutor {
     @Override
     public void start() {
         this.started = true;
+        LOGGER.info("ThreadManager of type 'VirtualThreadPerTask' started");
     }
 
     @Override
@@ -39,7 +49,7 @@ public class VirtualThreadPoolJobRunrExecutor implements JobRunrExecutor {
         }
     }
 
-    ExecutorService createVirtualThreadExecutorService() {
+    static ExecutorService createVirtualThreadExecutorService() {
         try {
             Method newVirtualThreadPerTaskExecutor = getMethod(Executors.class, "newVirtualThreadPerTaskExecutor");
             return (ExecutorService) newVirtualThreadPerTaskExecutor.invoke(null);
