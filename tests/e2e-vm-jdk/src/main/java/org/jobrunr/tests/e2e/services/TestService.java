bc6df57104f6e43ca796665fc3a8b04da8593939
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2024-02-20 21:36:50 +0100

    More tests related to #942.

--- tests/e2e-vm-jdk/src/main/java/org/jobrunr/tests/e2e/services/TestService.java
+++ tests/e2e-vm-jdk/src/main/java/org/jobrunr/tests/e2e/services/TestService.java
@@ -1,5 +1,10 @@
 package org.jobrunr.tests.e2e.services;
 
+import org.jobrunr.jobs.context.JobContext;
+import org.jobrunr.scheduling.BackgroundJob;
+
+import java.util.UUID;
+
 public class TestService {
 
     public void doWork() {
@@ -8,6 +13,17 @@ public class TestService {
 
     public void doWork(Work work) {
         System.out.println("This is a test service " + work.getSomeId() + "; " + work.getSomeString() + "; " + work.getSomeInt() + "; " + work.getSomeLong());
+    }
+
+    public void doWorkWithMultipleParameters(JobContext jobContext, long someLongValue, Work work) {
+        System.out.println("This is a test service with an extra long value: " + someLongValue + "; Work: " + work.getSomeId() + "; " + work.getSomeString() + "; " + work.getSomeInt() + "; " + work.getSomeLong());
+    }
+
+    public void run() {
+        Work work = new Work(123, "some string", 456L, UUID.randomUUID());
+        long someLongValue = 789L;
+
+        BackgroundJob.enqueue(() -> this.doWorkWithMultipleParameters(JobContext.Null, someLongValue, work));
     }
 
 }
