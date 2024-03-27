36827dbae8f1d37a53f0dc2236c2cc58e4e838e3
rdehuyss <ronald.dehuysser@gmail.com>
2024-03-27 19:32:34 +0100

    Improvements

--- framework-support/jobrunr-spring-boot-3-starter/src/main/java/org/jobrunr/spring/autoconfigure/storage/JobRunrElasticSearchStorageAutoConfiguration.java
+++ framework-support/jobrunr-spring-boot-3-starter/src/main/java/org/jobrunr/spring/autoconfigure/storage/JobRunrElasticSearchStorageAutoConfiguration.java
@@ -2,6 +2,7 @@ package org.jobrunr.spring.autoconfigure.storage;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.spring.autoconfigure.JobRunrAutoConfiguration;
 import org.jobrunr.spring.autoconfigure.JobRunrProperties;
 import org.jobrunr.storage.StorageProvider;
 import org.jobrunr.storage.StorageProviderUtils;
@@ -13,7 +14,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@AutoConfiguration(after = ElasticsearchRestClientAutoConfiguration.class)
+@AutoConfiguration(after = ElasticsearchRestClientAutoConfiguration.class, before = JobRunrAutoConfiguration.class)
 @ConditionalOnBean(ElasticsearchClient.class)
 @ConditionalOnProperty(prefix = "org.jobrunr.database", name = "type", havingValue = "elasticsearch", matchIfMissing = true)
 public class JobRunrElasticSearchStorageAutoConfiguration {
--- framework-support/jobrunr-spring-boot-3-starter/src/main/java/org/jobrunr/spring/autoconfigure/storage/JobRunrLettuceRedisClientStorageAutoConfiguration.java
+++ framework-support/jobrunr-spring-boot-3-starter/src/main/java/org/jobrunr/spring/autoconfigure/storage/JobRunrLettuceRedisClientStorageAutoConfiguration.java
@@ -2,6 +2,7 @@ package org.jobrunr.spring.autoconfigure.storage;
 
 import io.lettuce.core.RedisClient;
 import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.spring.autoconfigure.JobRunrAutoConfiguration;
 import org.jobrunr.spring.autoconfigure.JobRunrProperties;
 import org.jobrunr.storage.StorageProvider;
 import org.jobrunr.storage.nosql.redis.LettuceRedisStorageProvider;
@@ -9,9 +10,10 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@AutoConfiguration
+@AutoConfiguration(before = JobRunrAutoConfiguration.class)
 @ConditionalOnBean(RedisClient.class)
 @ConditionalOnProperty(prefix = "org.jobrunr.database", name = "type", havingValue = "redis-lettuce", matchIfMissing = true)
 public class JobRunrLettuceRedisClientStorageAutoConfiguration {
