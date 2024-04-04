0a0205494e6a5977125396c0754c1e9b4df23300
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2024-04-04 18:44:56 +0200

    Fix VersionNumber (#980)
    * Small fix
    
    * Fix build

--- core/src/test/java/org/jobrunr/storage/sql/mysql/MySqlDialectTest.java
+++ core/src/test/java/org/jobrunr/storage/sql/mysql/MySqlDialectTest.java
@@ -26,4 +26,10 @@ class MySqlDialectTest {
         assertThat(new MySqlDialect("MySQL", "8.0.1").selectForUpdateSkipLocked())
                 .isEqualTo(" FOR UPDATE SKIP LOCKED");
     }
+
+    @Test
+    void mySQL830DoesSupportSelectForUpdateSkipLocked() {
+        assertThat(new MySqlDialect("MySQL", "8.3.0").selectForUpdateSkipLocked())
+                .isEqualTo(" FOR UPDATE SKIP LOCKED");
+    }
 }
