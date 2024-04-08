d85b5b700d57cbbb39ef4676b156a17ec327f685
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2024-04-08 13:50:24 +0200

    Disable testIsolation

--- core/src/main/resources/org/jobrunr/dashboard/frontend/cypress.config.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/cypress.config.js
@@ -3,7 +3,8 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
     e2e: {
         supportFile: false,
-        specPattern: 'cypress/integration/*.js'
+        specPattern: 'cypress/integration/*.js',
+        testIsolation: false
     },
     video: false
 })
