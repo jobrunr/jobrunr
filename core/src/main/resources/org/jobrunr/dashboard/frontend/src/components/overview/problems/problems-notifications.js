ed2f5a8d9e80fcd965683422dc2d40c877f4a257
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2024-04-08 13:59:54 +0200

    Make problem notifications more noticeable on Dashboard (#981)
    * Make problem notifications more noticeable on Dashboard
    
    * Remove unused parameter
    
    * Allow to dismiss new version notification
    
    * Only load app once and retry on failure
    
    * Update jobs.spec.js
    
    * Disable testIsolation

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/overview/problems/problems-notifications.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/overview/problems/problems-notifications.js
@@ -1,16 +1,17 @@
-import {useEffect, useState} from 'react';
+import {useContext, useEffect, useMemo} from 'react';
 import LoadingIndicator from "../../LoadingIndicator";
 import JobNotFoundProblem from "./job-not-found-problem";
 import SevereJobRunrExceptionProblem from "./severe-jobrunr-exception-problem";
 import Grid from '@mui/material/Grid';
 import CpuAllocationIrregularityProblem from "./cpu-allocation-irregularity-problem";
 import NewJobRunrVersionAvailable from "./new-jobrunr-version-available";
-import JobRunrApiNotification from "./jobrunr-api-notification";
 import PollIntervalInSecondsIsTooSmallProblem from "./poll-interval-timebox-is-too-small-problem";
+import {ProblemsContext} from "../../../ProblemsContext";
+import JobRunrApiNotification from "./jobrunr-api-notification";
 
 const problemTypeOrder = {
-    "severe-jobrunr-exception": 0, "jobs-not-found": 1, "cpu-allocation-irregularity": 2,
-    "poll-interval-in-seconds-is-too-small": 3, "new-jobrunr-version": 4
+    "api-notification": 0, "severe-jobrunr-exception": 1, "jobs-not-found": 2, "cpu-allocation-irregularity": 3,
+    "poll-interval-in-seconds-is-too-small": 4, "new-jobrunr-version": 5
 };
 const getProblemOrder = (type) => problemTypeOrder[type] ?? 5;
 const problemCompareFn = (a, b) => {
@@ -18,54 +19,47 @@ const problemCompareFn = (a, b) => {
 }
 
 const Problems = () => {
-    const [isProblemsApiLoading, setProblemsApiIsLoading] = useState(true);
-    const [problems, setProblems] = useState([]);
-    const [mustRefresh, setRefresh] = useState(true);
+    const {problems, reload, isLoading} = useContext(ProblemsContext);
 
-    const refresh = () => setRefresh(!mustRefresh);
+    const sortedProblems = useMemo(() => {
+        return [...problems].sort(problemCompareFn)
+    }, [problems]);
 
     useEffect(() => {
-        fetch(`/api/problems`)
-            .then(res => res.json())
-            .then(response => {
-                response.sort(problemCompareFn);
-                setProblems(response);
-                setProblemsApiIsLoading(false);
-            })
-            .catch(error => console.log(error));
-    }, [mustRefresh]);
+        reload();
+    }, [reload]);
 
     return (
         <div style={{display: "flex"}}>
-            {isProblemsApiLoading
+            {isLoading
                 ? <LoadingIndicator/>
                 : <Grid container>
-                    <Grid item xs={12}><JobRunrApiNotification/></Grid>
-                    {problems.map((problem) => {
+                    {sortedProblems.map((problem) => {
                         switch (problem.type) {
+                            case "api-notification":
+                                return <Grid item xs={12} key={problem.type}><JobRunrApiNotification problem={problem}/></Grid>
                             case 'jobs-not-found':
                                 return <Grid item xs={12} key={problem.type}>
                                     <JobNotFoundProblem problem={problem}/>
                                 </Grid>
                             case 'severe-jobrunr-exception':
                                 return <Grid item xs={12} key={problem.type}>
-                                    <SevereJobRunrExceptionProblem problem={problem} refresh={refresh}/>
+                                    <SevereJobRunrExceptionProblem problem={problem} refresh={reload}/>
                                 </Grid>
                             case 'cpu-allocation-irregularity':
                                 return <Grid item xs={12} key={problem.type}>
-                                    <CpuAllocationIrregularityProblem problem={problem} refresh={refresh}/>
+                                    <CpuAllocationIrregularityProblem problem={problem} refresh={reload}/>
                                 </Grid>
                             case 'poll-interval-in-seconds-is-too-small':
                                 return <Grid item xs={12} key={problem.type}>
-                                    <PollIntervalInSecondsIsTooSmallProblem problem={problem} refresh={refresh}/>
+                                    <PollIntervalInSecondsIsTooSmallProblem problem={problem} refresh={reload}/>
                                 </Grid>
+                            case 'new-jobrunr-version':
+                                return <Grid item xs={12} key={problem.type}><NewJobRunrVersionAvailable problem={problem}/></Grid>
                             default:
                                 return <Grid item xs={12} key="unknown-problem">Unknown error</Grid>
                         }
                     })}
-                    <Grid item xs={12}>
-                        <NewJobRunrVersionAvailable/>
-                    </Grid>
                 </Grid>
             }
         </div>
