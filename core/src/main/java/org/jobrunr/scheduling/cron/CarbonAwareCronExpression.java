11b2941bc0c012f60968319f311dc8a2b11e40c2
Giorgos Skourtsidis <gskourts@gmail.com>
2024-05-23 18:59:53 +0300

    - CarbonAwareCronExpression: improve readability - WIP: add CarbonAwareCronExpressionTest

--- core/src/main/java/org/jobrunr/scheduling/cron/CarbonAwareCronExpression.java
+++ core/src/main/java/org/jobrunr/scheduling/cron/CarbonAwareCronExpression.java
@@ -31,20 +31,34 @@ public class CarbonAwareCronExpression extends Schedule {
     }
 
     public static CarbonAwareCronExpression create(String expression) {
+        String[] fields = parseAndValidateExpression(expression);
+        CronMargins cronMargins = parseCronMargins(fields);
+        validateTotalDuration(cronMargins.before, cronMargins.after);
+        CronExpression cronExpression = buildCronExpression(fields);
+        validateDurationBetweenRuns(cronExpression, cronMargins.before, cronMargins.after);
+        return new CarbonAwareCronExpression(cronExpression, cronMargins.before, cronMargins.after);
+    }
+
+    private static String[] parseAndValidateExpression(String expression) {
         String[] fields = expression.trim().toLowerCase().split("\\s+");
         if (expression.isEmpty()) {
             throw new InvalidCarbonAwareCronExpressionException("Empty carbon aware cron expression");
         }
         int count = fields.length;
-        if (count > 8 || count < 7) {
+        if (count != 8 && count != 7) {
             throw new InvalidCarbonAwareCronExpressionException(
                     "Carbon aware crontab expression should have 8 fields (seconds resolution) or 7 fields (minutes resolution).\n" +
                             "Also, last 2 fields has to be parseable as java.time.Duration objects (ISO-8601 PnDTnHnMn.nS format)\n" +
                             "Example:\n " +
                             "`0 0 * * * PT2H PT6H`: run every day at midnight. Job can be scheduled 2 hours before and 6 hours after midnight, in the time when carbon emissions are the lowest.");
         }
+        return fields;
+    }
+
+    private static CronMargins parseCronMargins(String[] fields) {
         Duration allowedBefore;
         Duration allowedAfter;
+        int count = fields.length;
         try {
             allowedBefore = Duration.parse(fields[count - 2]);
             allowedAfter = Duration.parse(fields[count - 1]);
@@ -52,43 +66,43 @@ public class CarbonAwareCronExpression extends Schedule {
             throw new InvalidCarbonAwareCronExpressionException("Last 2 fields has to be parseable as java.time.Duration objects (ISO-8601 PnDTnHnMn.nS format)" +
                     "Provided: " + fields[count - 2] + " and " + fields[count - 1] + "\n" +
                     "Valid Duration Examples:\n " +
-                    "- PT20.345S represents 20.345 seconds, " +
                     "- PT15M represents 15 minutes, " +
                     "- PT10H represents 10 hours, " +
                     "- P2D represents 2 days, " +
+                    "- PT20.345S represents 20.345 seconds, " +
                     "- P2DT3H4M represents 2 days, 3 hours and 4 minutes");
         }
+        return new CronMargins(allowedBefore, allowedAfter);
+    }
 
+    private static void validateTotalDuration(Duration allowedBefore, Duration allowedAfter) {
         if (allowedBefore.plus(allowedAfter).toHours() < 3) {
             throw new InvalidCarbonAwareCronExpressionException("Allowed duration before and after must be at least 3 hours, in order to schedule the job at a time when carbon emission from electricity are low. " +
-                    "If you need an interval of less than 3 hours, you can use normal recurring job scheduling.\n" +
-                    "Provided: " + allowedBefore + " and " + allowedAfter);
+                    "Provided: " + allowedBefore + " and " + allowedAfter + ".\n" +
+                    "If you need an interval of less than 3 hours, you can use normal recurring job scheduling.");
         }
+    }
 
+    private static CronExpression buildCronExpression(String[] fields) {
         StringBuilder cronExpressionStrBuilder = new StringBuilder();
         for (int i = 0; i < fields.length - 2; i++) {
             cronExpressionStrBuilder.append(fields[i]).append(" ");
         }
-        String cronExpressionStr = cronExpressionStrBuilder.toString().trim();
-        CronExpression cronExpression = CronExpression.create(cronExpressionStr);
+        return CronExpression.create(cronExpressionStrBuilder.toString().trim());
+    }
+
+
+    private static void validateDurationBetweenRuns(CronExpression cronExpression, Duration allowedBefore, Duration allowedAfter) {
         Instant baseTime = Instant.now();
         Instant nextTime = cronExpression.next(baseTime, baseTime, ZoneId.systemDefault()).getInstant();
         Instant nextNextTime = cronExpression.next(baseTime, nextTime, ZoneId.systemDefault()).getInstant();
         if (Duration.between(nextTime, nextNextTime).compareTo(allowedBefore.plus(allowedAfter)) < 0) {
             throw new InvalidCarbonAwareCronExpressionException("Duration between 2 runs should be equal or greater than (allowedDurationBefore + allowedDurationAfter)\n" +
-                    String.format("Provided: %s %s %s\n", cronExpressionStr, allowedBefore, allowedAfter) +
+                    String.format("Provided: %s %s %s\n", cronExpression, allowedBefore, allowedAfter) +
                     "Duration between 2 runs: " + Duration.between(nextTime, nextNextTime));
-
         }
-        return new CarbonAwareCronExpression(cronExpression, allowedBefore, allowedAfter);
     }
 
-    /**
-     * Calculates the next occurrence based on provided base time.
-     *
-     * @param createdAtInstant Instant object based on which calculating the next occurrence.
-     * @return {@link RecurringJobNextRun} object containing the next CarbonAwarePeriod.
-     */
     @Override
     public RecurringJobNextRun next(Instant createdAtInstant, Instant currentInstant, ZoneId zoneId) {
         Instant nextPossibleTime = cronExpression.next(createdAtInstant, currentInstant, zoneId).getInstant();
@@ -131,6 +145,17 @@ public class CarbonAwareCronExpression extends Schedule {
         return this.cronExpression.equals(carbonAwareCronExpression.cronExpression) &&
                 this.allowedDurationBefore.equals(carbonAwareCronExpression.allowedDurationBefore) &&
                 this.allowedDurationAfter.equals(carbonAwareCronExpression.allowedDurationAfter);
+    }
+
+    private static class CronMargins {
+        // possible names: ScheduleWindow, CronInterval, FlexWindow
+        final Duration before;
+        final Duration after;
+
+        CronMargins(Duration before, Duration after) {
+            this.before = before;
+            this.after = after;
+        }
     }
 
     // TESTS:
