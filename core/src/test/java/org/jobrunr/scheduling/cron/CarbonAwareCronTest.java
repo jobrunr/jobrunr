004dcf1310d4e015faa5186d424ac62447ea116d
Giorgos Skourtsidis <gskourts@gmail.com>
2024-05-28 14:23:42 +0300

    add tests to CarbonAwareCronTest

--- core/src/test/java/org/jobrunr/scheduling/cron/CarbonAwareCronTest.java
+++ core/src/test/java/org/jobrunr/scheduling/cron/CarbonAwareCronTest.java
@@ -22,13 +22,20 @@ public class CarbonAwareCronTest {
     static Stream<Arguments> getCarbonAwareCronExpressionsAndTheirValue() {
         return Stream.of(
                 // Daily
+                arguments(CarbonAwareCron.daily(1, 2), "0 0 * * * PT1H PT2H"),
+                arguments(CarbonAwareCron.daily(15, 3, 6), "0 15 * * * PT3H PT6H"),
                 arguments(CarbonAwareCron.daily(Duration.ofHours(1), Duration.ofHours(2)), "0 0 * * * PT1H PT2H"),
                 arguments(CarbonAwareCron.daily(3, Duration.ofHours(1), Duration.ofHours(2)), "0 3 * * * PT1H PT2H"),
                 arguments(CarbonAwareCron.daily(1, 3, Duration.ofHours(1), Duration.ofHours(2)), "3 1 * * * PT1H PT2H"),
+                arguments(CarbonAwareCron.daily(1, 16, 1, 2), "16 1 * * * PT1H PT2H"),
                 arguments(CarbonAwareCron.dailyAllowedToRunEarlier(5, 3), "0 5 * * * PT3H PT0S"),
+                arguments(CarbonAwareCron.dailyAllowedToRunEarlier(5, 22, 3), "22 5 * * * PT3H PT0S"),
                 arguments(CarbonAwareCron.dailyAllowedToRunEarlier(3), "0 0 * * * PT3H PT0S"),
+                arguments(CarbonAwareCron.dailyAllowedToRunEarlier(Duration.ofMinutes(180)), "0 0 * * * PT3H PT0S"),
                 arguments(CarbonAwareCron.dailyAllowedToRunLater(5, 3), "0 5 * * * PT0S PT3H"),
+                arguments(CarbonAwareCron.dailyAllowedToRunLater(5, 12, 3), "12 5 * * * PT0S PT3H"),
                 arguments(CarbonAwareCron.dailyAllowedToRunLater(3), "0 0 * * * PT0S PT3H"),
+                arguments(CarbonAwareCron.dailyAllowedToRunLater(Duration.ofMinutes(180)), "0 0 * * * PT0S PT3H"),
                 arguments(CarbonAwareCron.dailyBefore(10), "0 0 * * * PT0S PT10H"),
                 arguments(CarbonAwareCron.dailyAfter(10), "0 10 * * * PT0S PT14H"),
                 arguments(CarbonAwareCron.dailyBetween(10, 20), "0 10 * * * PT0S PT10H"),
@@ -41,9 +48,19 @@ public class CarbonAwareCronTest {
 
                 // Monthly
                 arguments(CarbonAwareCron.monthly(1, 2), "0 0 1 * * PT24H PT48H"),
-                arguments(CarbonAwareCron.monthlyAllowedToRunEarlier(3), "0 0 1 * * PT72H PT0S"),
-                arguments(CarbonAwareCron.monthlyAllowedToRunLater(2), "0 0 1 * * PT0S PT48H"),
+                arguments(CarbonAwareCron.monthly(3, 1, 2), "0 0 3 * * PT24H PT48H"),
                 arguments(CarbonAwareCron.monthly(15, 3, 2, 1), "0 3 15 * * PT48H PT24H"),
+                arguments(CarbonAwareCron.monthly(15, Duration.ofHours(2), Duration.ofHours(3)), "0 0 15 * * PT2H PT3H"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunEarlier(3), "0 0 1 * * PT72H PT0S"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunEarlier(18, 3), "0 0 18 * * PT72H PT0S"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunEarlier(18, 12, 3), "0 12 18 * * PT72H PT0S"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunEarlier(18, 12, 48, 3), "48 12 18 * * PT72H PT0S"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunHoursEarlier(18, 12, 48, 3), "48 12 18 * * PT3H PT0S"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunLater(2), "0 0 1 * * PT0S PT48H"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunLater(9, 2), "0 0 9 * * PT0S PT48H"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunLater(9, 22, 2), "0 22 9 * * PT0S PT48H"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunLater(9, 22, 50, 2), "50 22 9 * * PT0S PT48H"),
+                arguments(CarbonAwareCron.monthlyAllowedToRunHoursLater(3, 22, 50, 10), "50 22 3 * * PT0S PT10H"),
 
                 // Yearly
                 arguments(CarbonAwareCron.yearly(5, 1, 2), "0 0 1 5 * PT720H PT1464H"),
