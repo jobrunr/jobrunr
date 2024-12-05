a6769c8313fe834964b04c515281757e381c5084
rdehuyss <ronald.dehuysser@gmail.com>
2024-12-05 10:41:59 +0100

    Make sure we use correct casing in all SQL Scripts.

--- core/src/test/java/org/jobrunr/storage/sql/SqlScriptsTest.java
+++ core/src/test/java/org/jobrunr/storage/sql/SqlScriptsTest.java
@@ -9,7 +9,6 @@ import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -17,41 +16,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SqlScriptsTest {
 
-    public static final Map<String, Class<?>> TABLES_WITH_FIELDS = Map.of(
-            StorageProviderUtils.Migrations.NAME, StorageProviderUtils.Migrations.class,
-            StorageProviderUtils.Metadata.NAME, StorageProviderUtils.Metadata.class,
-            StorageProviderUtils.Jobs.NAME, StorageProviderUtils.Jobs.class,
-            StorageProviderUtils.RecurringJobs.NAME, StorageProviderUtils.RecurringJobs.class,
-            StorageProviderUtils.BackgroundJobServers.NAME.replace("_", ""), StorageProviderUtils.BackgroundJobServers.class,
-            StorageProviderUtils.JobStats.NAME, StorageProviderUtils.JobStats.class);
-
     @Test
     public void testSqlScriptsHaveCorrectColumnNamesAndCasing() throws Exception {
         List<FileWithContent> allSqlFiles = getAllSqlFiles();
 
-        allSqlFiles.stream()
-                .filter(this::isNotAnEmptyMigration)
-                .forEach(this::testNamingAndCasing);
+        allSqlFiles.forEach(this::testNamingAndCasing);
     }
 
     private void testNamingAndCasing(FileWithContent fileWithContent) {
-        List<String> relevantTablesWithFields = TABLES_WITH_FIELDS.keySet().stream()
-                .filter(tableName -> fileWithContent.content.toLowerCase().contains("jobrunr_" + tableName))
-                .collect(toList());
-
-        assertThat(relevantTablesWithFields)
-                .describedAs("Did not find a matching table for %s", fileWithContent.path)
-                .isNotEmpty();
-
-        relevantTablesWithFields.forEach(tableName -> testNamingAndCasingForAllFieldsDefinedIn(fileWithContent, TABLES_WITH_FIELDS.get(tableName)));
+        testNamingAndCasingForAllFieldsDefinedIn(fileWithContent, StorageProviderUtils.Jobs.class);
+        testNamingAndCasingForAllFieldsDefinedIn(fileWithContent, StorageProviderUtils.RecurringJobs.class);
+        testNamingAndCasingForAllFieldsDefinedIn(fileWithContent, StorageProviderUtils.BackgroundJobServers.class);
     }
 
     private void testNamingAndCasingForAllFieldsDefinedIn(FileWithContent fileWithContent, Class<?> clazz) {
-        Field[] fields = clazz.getDeclaredFields();
+        Field[] fields = clazz.getDeclaredFields();   // Get public fields (only public ones)
 
         for (Field field : fields) {
             try {
-                if (isNotPublicStatic(field)) continue;
+                int modifiers = field.getModifiers();
+                if (!(Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers))) continue;
 
                 String fieldName = field.getName();
                 String fieldValue = field.get(clazz).toString();
@@ -62,30 +46,19 @@ public class SqlScriptsTest {
         }
     }
 
-    private static boolean isNotPublicStatic(Field field) {
-        int modifiers = field.getModifiers();
-        return !(Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers));
-    }
-
     private void testNamingAndCasingForSqlField(FileWithContent fileWithContent, String storageProviderReference, String fieldValue) {
-        if (!fileWithContent.content.toLowerCase().contains(fieldValue.toLowerCase())) return;
+        if (!fileWithContent.getContent().toLowerCase().contains(fieldValue.toLowerCase())) return;
 
-        int countWithoutCasing = countOccurrences(fileWithContent.content.toLowerCase(), fieldValue.toLowerCase());
-        int countWithCasing = countOccurrences(fileWithContent.content, fieldValue);
+        int countWithoutCasing = fileWithContent.getContent().toLowerCase().split("\\b" + fieldValue.toLowerCase() + "\\b").length - 1;
+        int countWithCasing = fileWithContent.getContent().split("\\b" + fieldValue + "\\b").length - 1;
         assertThat(countWithoutCasing)
-                .describedAs("Found invalid casing in " + fileWithContent.path + " for field " + storageProviderReference + " with value " + fieldValue)
+                .describedAs("Found invalid casing in " + fileWithContent.getPath() + " for field " + storageProviderReference + " with value " + fieldValue)
                 .isEqualTo(countWithCasing);
     }
 
-    private int countOccurrences(String text, String substringToCount) {
-        return text.split("(?<!')\\b" + substringToCount + "\\b(?!')").length - 1;
-    }
-
-    private boolean isNotAnEmptyMigration(FileWithContent fileWithContent) {
-        return !fileWithContent.content.startsWith("-- Empty migration");
-    }
 
     public List<FileWithContent> getAllSqlFiles() throws Exception {
+        // Walk through all files in the directory
         try (Stream<Path> paths = Files.walk(Path.of("src/main/resources/org/jobrunr/storage/sql"))) {
             return paths.filter(Files::isRegularFile)
                     .filter(path -> path.getFileName().toString().endsWith(".sql"))
@@ -105,6 +78,14 @@ public class SqlScriptsTest {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        public Path getPath() {
+            return path;
+        }
+
+        public String getContent() {
+            return content;
         }
     }
 }
