997b10deaa3b7d511853b8decf68e9fe0c24aa4f
Devashish Taneja <32792385+devashishTaneja@users.noreply.github.com>
2025-01-31 14:46:21 +0530

    [Feature] Automatically create jobs if Spring Bean is annotated with @AsyncJob Annotation (#1181)
    * [Feature] Automatically create jobs if Spring Bean is annotated with @Job annotation #977
    
    * Feature#977
    
    * Feature#977
    
    * Feature#977
    
    * Use ProxyFactory instead of Byte Buddy
    
    * Refactor for coding practices
    
    * Refactor for coding practices
    
    * Add integration test
    
    * Add support for parameters
    
    * - Remove double instance of AsyncPostProcessor
    - Optimise test case using JobRunrAssertions
    
    * Improvements related to return types and nested jobs (similar to @Async of Spring)
    
    * Small improvement related to import
    
    ---------
    
    Co-authored-by: rdehuyss <ronald.dehuysser@gmail.com>

--- framework-support/jobrunr-spring-boot-2-starter/src/main/java/org/jobrunr/spring/autoconfigure/JobRunrAutoConfiguration.java
+++ framework-support/jobrunr-spring-boot-2-starter/src/main/java/org/jobrunr/spring/autoconfigure/JobRunrAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.jobrunr.dashboard.JobRunrDashboardWebServerConfiguration;
 import org.jobrunr.jobs.details.JobDetailsGenerator;
 import org.jobrunr.jobs.filters.RetryFilter;
 import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.scheduling.AsyncJobPostProcessor;
 import org.jobrunr.scheduling.JobRequestScheduler;
 import org.jobrunr.scheduling.JobScheduler;
 import org.jobrunr.scheduling.RecurringJobPostProcessor;
@@ -162,6 +163,12 @@ public class JobRunrAutoConfiguration {
     @ConditionalOnBean(JobScheduler.class)
     public static RecurringJobPostProcessor recurringJobPostProcessor() {
         return new RecurringJobPostProcessor();
+    }
+
+    @Bean
+    @ConditionalOnBean(JobScheduler.class)
+    public static AsyncJobPostProcessor asyncJobPostProcessor() {
+        return new AsyncJobPostProcessor();
     }
 
     @Configuration
