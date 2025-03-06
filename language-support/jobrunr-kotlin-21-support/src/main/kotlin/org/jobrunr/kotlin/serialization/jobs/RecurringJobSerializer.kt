215cc51c2515038d55128edfcc8a80b65ce55bd6
SIMULATAN <jakob@simulatan.me>
2025-03-06 16:22:07 +0100

    fixup! Rewrite `KRecurringJob` to a dedicated serializer

--- language-support/jobrunr-kotlin-21-support/src/main/kotlin/org/jobrunr/kotlin/serialization/jobs/RecurringJobSerializer.kt
+++ language-support/jobrunr-kotlin-21-support/src/main/kotlin/org/jobrunr/kotlin/serialization/jobs/RecurringJobSerializer.kt
@@ -1,11 +1,15 @@
 package org.jobrunr.kotlin.serialization.jobs
 
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.SetSerializer
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.ClassSerialDescriptorBuilder
+import kotlinx.serialization.descriptors.SerialKind
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.buildSerialDescriptor
 import kotlinx.serialization.encoding.*
 import org.jobrunr.jobs.JobDetails
 import org.jobrunr.jobs.RecurringJob
@@ -13,6 +17,9 @@ import org.jobrunr.kotlin.serialization.utils.ClassDiscriminatedContextualSerial
 
 @OptIn(ExperimentalSerializationApi::class)
 object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedContextualSerializer.PolymorphicContinuationSerializer<RecurringJob> {
+	@OptIn(InternalSerializationApi::class)
+	val createdByDescriptor = buildSerialDescriptor(RecurringJob.CreatedBy ::class.qualifiedName!!, SerialKind.ENUM)
+
 	val buildClassSerialDescriptorElements = fun ClassSerialDescriptorBuilder.() {
 		element("id", String.serializer().descriptor)
 		element("version", Int.serializer().descriptor)
@@ -22,6 +29,7 @@ object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedCon
 		element("jobDetails", JobDetailsSerializer.descriptor)
 		element("scheduleExpression", String.serializer().descriptor)
 		element("zoneId", String.serializer().descriptor)
+		element("createdBy", createdByDescriptor)
 		element("createdAt", String.serializer().descriptor)
 	}
 	
@@ -36,11 +44,12 @@ object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedCon
 		encodeIntElement(descriptor, 1, value.version)
 		encodeStringElement(descriptor, 2, value.jobName)
 		encodeNullableSerializableElement(descriptor, 3, Int.serializer(), value.amountOfRetries)
-		encodeSerializableElement(descriptor, 4, SetSerializer(String.serializer()), value.labels)
+		encodeSerializableElement(descriptor, 4, ListSerializer(String.serializer()), value.labels)
 		encodeSerializableElement(descriptor, 5, JobDetailsSerializer, value.jobDetails)
 		encodeStringElement(descriptor, 6, value.scheduleExpression)
 		encodeStringElement(descriptor, 7, value.zoneId)
-		encodeStringElement(descriptor, 8, value.createdAt.toString())
+		encodeStringElement(descriptor, 8, value.createdBy.name)
+		encodeStringElement(descriptor, 9, value.createdAt.toString())
 	}
 
 	override fun deserialize(decoder: Decoder): RecurringJob = decoder.decodeStructure(descriptor) {
@@ -48,10 +57,11 @@ object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedCon
 		var version = -1
 		lateinit var jobName: String
 		var amountOfRetries: Int? = null
-		lateinit var labels: Set<String>
+		lateinit var labels: List<String>
 		lateinit var jobDetails: JobDetails
 		lateinit var scheduleExpression: String
 		lateinit var zoneId: String
+		lateinit var createdBy: RecurringJob.CreatedBy
 		lateinit var createdAt: String
 
 		while (true) {
@@ -61,11 +71,12 @@ object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedCon
 				1 -> version = decodeIntElement(descriptor, 1)
 				2 -> jobName = decodeStringElement(descriptor, 2)
 				3 -> amountOfRetries = decodeNullableSerializableElement(descriptor, 3, Int.serializer())
-				4 -> labels = decodeSerializableElement(descriptor, 4, SetSerializer(String.serializer()))
+				4 -> labels = decodeSerializableElement(descriptor, 4, ListSerializer(String.serializer()))
 				5 -> jobDetails = decodeSerializableElement(descriptor, 5, JobDetailsSerializer)
 				6 -> scheduleExpression = decodeStringElement(descriptor, 6)
 				7 -> zoneId = decodeStringElement(descriptor, 7)
-				8 -> createdAt = decodeStringElement(descriptor, 8)
+				8 -> createdBy = RecurringJob.CreatedBy.valueOf(decodeStringElement(createdByDescriptor, 8))
+				9 -> createdAt = decodeStringElement(descriptor, 9)
 				else -> error("Unexpected index $index")
 			}
 		}
@@ -76,6 +87,7 @@ object RecurringJobSerializer : KSerializer<RecurringJob>, ClassDiscriminatedCon
 			jobDetails,
 			scheduleExpression,
 			zoneId,
+			createdBy,
 			createdAt
 		).apply {
 			this.jobName = jobName
