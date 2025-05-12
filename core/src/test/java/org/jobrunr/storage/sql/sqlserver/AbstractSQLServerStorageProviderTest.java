6116e30146dc71270f077763d852616d0a83d8dc
rdehuyss <ronald.dehuysser@gmail.com>
2025-05-12 10:22:58 +0200

    Improvements to align with Pro

--- core/src/test/java/org/jobrunr/storage/sql/sqlserver/AbstractSQLServerStorageProviderTest.java
+++ core/src/test/java/org/jobrunr/storage/sql/sqlserver/AbstractSQLServerStorageProviderTest.java
@@ -2,12 +2,14 @@ package org.jobrunr.storage.sql.sqlserver;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.jobrunr.storage.sql.SqlStorageProviderTest;
+import org.jobrunr.utils.CIInfo;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.extension.AfterAllSubclasses;
 import org.junit.jupiter.extension.BeforeAllSubclasses;
 import org.junit.jupiter.extension.ForAllSubclassesExtension;
 import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
 import java.time.Duration;
@@ -15,11 +17,12 @@ import java.time.Instant;
 
 import static java.time.Instant.now;
 import static org.jobrunr.storage.sql.SqlTestUtils.toHikariDataSource;
+import static org.jobrunr.utils.CIInfo.CIType.NAS;
 
 @ExtendWith(ForAllSubclassesExtension.class)
 public abstract class AbstractSQLServerStorageProviderTest extends SqlStorageProviderTest {
 
-    protected static MSSQLServerContainer sqlContainer = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:2022-latest");
+    protected static MSSQLServerContainer sqlContainer = new MSSQLServerContainer<>(getSqlServerDockerImageName());
 
     protected static HikariDataSource dataSource;
 
@@ -47,5 +50,13 @@ public abstract class AbstractSQLServerStorageProviderTest extends SqlStoragePro
     @AfterAllSubclasses
     public static void stopSqlContainer() {
         sqlContainer.stop();
+    }
+
+    private static DockerImageName getSqlServerDockerImageName() {
+        if (CIInfo.isRunningOn(NAS)) {
+            return DockerImageName.parse("mcr.microsoft.com/azure-sql-edge").asCompatibleSubstituteFor("mcr.microsoft.com/mssql/server");
+        }
+
+        return DockerImageName.parse("mcr.microsoft.com/mssql/server:2022-latest");
     }
 }
