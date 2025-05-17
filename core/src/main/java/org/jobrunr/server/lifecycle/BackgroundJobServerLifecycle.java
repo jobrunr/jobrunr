1b79cf7f8fbe9dbdadcbff43da27f383392c5a06
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2025-05-17 17:47:32 +0200

    Improve logging on Drone CI (#1271)
    * Improve gradle logging
    * Improve BackgroundJobServer Lifecycle to prevent lock contention

---
+++ core/src/main/java/org/jobrunr/server/lifecycle/BackgroundJobServerLifecycle.java
@@ -0,0 +1,43 @@
+package org.jobrunr.server.lifecycle;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class BackgroundJobServerLifecycle {
+
+    final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    final Condition readWriteFinished = readWriteLock.writeLock().newCondition();
+    volatile BackgroundJobServerLifecycleEvent lifecycleEvent;
+    volatile boolean isRunning = false;
+
+    public LifecycleChangeLock goTo(BackgroundJobServerLifecycleEvent event) {
+        return new LifecycleChangeLock(this, event);
+    }
+
+    public LifecycleReadLock readLock() {
+        return new LifecycleReadLock(this);
+    }
+
+    public boolean isTransitioning() {
+        return readWriteLock.isWriteLocked();
+    }
+
+    public boolean isTransitioningTo(BackgroundJobServerLifecycleEvent event) {
+        if (!isTransitioning()) return false;
+
+        try {
+            if (event == null) {
+                readWriteFinished.await(10, TimeUnit.MILLISECONDS);
+            }
+            return Objects.equals(event, lifecycleEvent);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+}
---
+++ core/src/main/java/org/jobrunr/server/lifecycle/LifecycleChangeLock.java
@@ -0,0 +1,27 @@
+package org.jobrunr.server.lifecycle;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LifecycleChangeLock extends AbstractLifecycleLock {
+
+    public LifecycleChangeLock(BackgroundJobServerLifecycle backgroundJobServerLifecycle, BackgroundJobServerLifecycleEvent lifecycleEvent) {
+        super(backgroundJobServerLifecycle);
+        backgroundJobServerLifecycle.lifecycleEvent = lifecycleEvent;
+        backgroundJobServerLifecycle.readWriteFinished.signalAll();
+    }
+
+    public void succeeded() {
+        this.backgroundJobServerLifecycle.isRunning = backgroundJobServerLifecycle.lifecycleEvent.isRunning;
+    }
+
+    @Override
+    protected void lock(ReentrantReadWriteLock readWriteLock) {
+        if (readWriteLock.getReadHoldCount() > 0) throw new IllegalMonitorStateException("Cannot upgrade read to write lock");
+        readWriteLock.writeLock().lock();
+    }
+
+    @Override
+    protected void unlock(ReentrantReadWriteLock readWriteLock) {
+        readWriteLock.writeLock().unlock();
+    }
+}
