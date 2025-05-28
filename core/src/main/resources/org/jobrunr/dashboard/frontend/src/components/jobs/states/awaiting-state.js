8eda2caaeb20d7a372a5acdb8ee69fb229d54eee
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-05-28 13:23:46 +0200

    use SwitchableTimeAgo in awaiting-state

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/states/awaiting-state.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/states/awaiting-state.js
@@ -1,13 +1,12 @@
 import {JobState} from "./job-state";
+import {SwitchableTimeAgo} from "../../utils/time-ago";
 
 const Awaiting = ({jobState}) => {
-    const from = new Date(jobState.from);
-    const to = new Date(jobState.to);
     const title = "Job Pending " + (jobState.reason ? `- ${jobState.reason}` : "");
 
     return (
         <JobState state="awaiting" title={title} date={new Date(jobState.createdAt)}>
-            Job is awaiting optimal low-carbon execution window between {from.toString()} and {to.toString()}.
+            Job is awaiting optimal low-carbon execution window between <SwitchableTimeAgo date={new Date(jobState.from)}/> and <SwitchableTimeAgo date={new Date(jobState.to)}/>.
         </JobState>
     )
 };
