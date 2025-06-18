007257c15dacb12e84c200c540c68c6c85438c7a
rdehuyss <ronald.dehuysser@gmail.com>
2025-06-18 11:13:26 +0200

    Update Jackson

--- core/src/test/java/org/jobrunr/utils/JarUtilsTest.java
+++ core/src/test/java/org/jobrunr/utils/JarUtilsTest.java
@@ -25,7 +25,7 @@ class JarUtilsTest {
 
     @Test
     void jacksonVersion() {
-        assertThat(JarUtils.getVersion(ObjectMapper.class)).isEqualTo("2.18.3");
+        assertThat(JarUtils.getVersion(ObjectMapper.class)).isEqualTo("2.19.1");
     }
 
     @Test
