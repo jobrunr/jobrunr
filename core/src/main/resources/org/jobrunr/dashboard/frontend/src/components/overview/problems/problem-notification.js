5bbf8717a0ccc1f17ec3b318a782a8e0cd28d3ab
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2025-06-23 11:10:03 +0200

    Remove overview notifications

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/overview/problems/problem-notification.js
+++
@@ -1,8 +0,0 @@
-import {Alert, AlertTitle} from "@mui/material";
-
-export const ProblemNotification = ({title = "Warning", children, severity = "warning", ...rest}) => (
-    <Alert style={{marginBottom: '2rem'}} severity={severity} {...rest}>
-        <AlertTitle><h4 style={{lineHeight: 1, margin: 0}}>{title}</h4></AlertTitle>
-        {children}
-    </Alert>
-)
