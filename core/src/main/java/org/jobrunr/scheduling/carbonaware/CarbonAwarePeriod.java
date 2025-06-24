f4609410583961ea588d97ac441cc076ca0393a6
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-06-24 16:59:16 +0200

    added javadoc to carbonAwarePeriod

--- core/src/main/java/org/jobrunr/scheduling/carbonaware/CarbonAwarePeriod.java
+++ core/src/main/java/org/jobrunr/scheduling/carbonaware/CarbonAwarePeriod.java
@@ -1,5 +1,6 @@
 package org.jobrunr.scheduling.carbonaware;
 
+import org.jobrunr.jobs.RecurringJob;
 import org.jobrunr.utils.InstantUtils;
 
 import java.time.Instant;
@@ -33,14 +34,25 @@ public class CarbonAwarePeriod {
         return to;
     }
 
-    // TODO add javadoc
-    // TODO add test
+    /**
+     * Allows to relax schedule of a job to minimize carbon impact.
+     * The job will run before the provided {@code to} Temporal instance.
+     *
+     * @param to the time expressed in java.time.Temporal before which the job must be scheduled.
+     * @return A carbon aware period between {@code Instant.now()} and the provided {@code to}.
+     */
     public static CarbonAwarePeriod before(Temporal to) {
         return between(now(), to);
     }
 
-    // TODO add javadoc
-    // TODO add test
+    /**
+     * Allows to relax schedule of a job to minimize carbon impact.
+     * The job will run between the two provided {@code to} Temporal instances as the interval.
+     *
+     * @param from the start time expressed in java.time.Temporal of the carbon aware margin.
+     * @param to the end time expressed in java.time.Temporal of the carbon aware margin.
+     * @return A carbon aware period between the provided {@code from} and {@code to}.
+     */
     public static CarbonAwarePeriod between(Temporal from, Temporal to) {
         return new CarbonAwarePeriod(toInstant(from), toInstant(to));
     }
