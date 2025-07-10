f45a8c85caee658d0e2a830304701509c5d10bdb
Wouter <304593+wgroeneveld@users.noreply.github.com>
2025-07-10 11:56:46 +0200

    improve carbon aware scheduled message in case of disabled config based on the scheduledState (#1346)

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/notifications/carbon-aware-scheduled-notification.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/notifications/carbon-aware-scheduled-notification.js
@@ -1,17 +1,34 @@
 import {EnergySavingsLeaf} from "@mui/icons-material";
-import {SwitchableTimeRangeFormatter} from "../../utils/time-ago";
-import {isCarbonAwaitingState} from "../../utils/job-utils";
+import {SwitchableTimeFormatter, SwitchableTimeRangeFormatter} from "../../utils/time-ago";
 import {JobNotification} from "./job-notification";
 
-const CarbonAwareScheduledNotification = ({state}) => {
-    if (!isCarbonAwaitingState(state)) return;
+const CarbonAwareScheduledNotification = ({job}) => {
+    const carbonAwareAwaitingState = job?.jobHistory?.find(h => h["@class"].endsWith("CarbonAwareAwaitingState"));
+    if (!carbonAwareAwaitingState) return;
+    const scheduledState = job.jobHistory.find(h => h.state === "SCHEDULED");
+
+    function carbonAwareSchedulingDisabled() {
+        return scheduledState?.reason.indexOf("Carbon aware scheduling is disabled") >= 0;
+    }
+
+    if(carbonAwareSchedulingDisabled()) {
+        return (
+            <JobNotification
+                severity="info"
+                icon={<EnergySavingsLeaf fontSize="small" color="success"/>}
+            >
+                This job is scheduled Carbon Aware but Carbon Aware Processing is disabled: scheduling at <SwitchableTimeFormatter date={new Date(scheduledState.scheduledAt)}/>.
+            </JobNotification>
+
+        )
+    }
 
     return (
         <JobNotification
             severity="info"
             icon={<EnergySavingsLeaf fontSize="small" color="success"/>}
         >
-            This job is scheduled Carbon Aware <SwitchableTimeRangeFormatter from={new Date(state.from)} to={new Date(state.to)}/> to minimize carbon impact.
+            This job is scheduled Carbon Aware <SwitchableTimeRangeFormatter from={new Date(carbonAwareAwaitingState.from)} to={new Date(carbonAwareAwaitingState.to)}/> to minimize carbon impact.
         </JobNotification>
     )
 };
