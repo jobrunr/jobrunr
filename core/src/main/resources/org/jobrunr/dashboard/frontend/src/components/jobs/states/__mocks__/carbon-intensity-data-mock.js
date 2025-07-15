2abf7cb003772a78daf1cc33fd20d1371211b101
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-07-15 16:32:13 +0200

    add exemplary awaiting state carbon intensity char tests

---
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/states/__mocks__/carbon-intensity-data-mock.js
@@ -0,0 +1,247 @@
+
+export default function CarbonIntensityDataMock() {
+    return {
+        "apiResponse": {
+        "code": "OK",
+            "message": "DataProvider ENTSO-E and area Belgium has 46 forecasts."
+    },
+        "dataProvider": "ENTSO-E",
+        "dataIdentifier": "10YBE----------2",
+        "displayName": "Belgium",
+        "timezone": "Europe/Brussels",
+        "nextForecastAvailableAt": "2025-07-16T16:30:00Z",
+        "forecastInterval": 3600,
+        "intensityForecast": [
+        {
+            "periodStartAt": "2025-07-15T00:00:00Z",
+            "periodEndAt": "2025-07-15T01:00:00Z",
+            "rank": 25
+        },
+        {
+            "periodStartAt": "2025-07-15T01:00:00Z",
+            "periodEndAt": "2025-07-15T02:00:00Z",
+            "rank": 22
+        },
+        {
+            "periodStartAt": "2025-07-15T02:00:00Z",
+            "periodEndAt": "2025-07-15T03:00:00Z",
+            "rank": 19
+        },
+        {
+            "periodStartAt": "2025-07-15T03:00:00Z",
+            "periodEndAt": "2025-07-15T04:00:00Z",
+            "rank": 26
+        },
+        {
+            "periodStartAt": "2025-07-15T04:00:00Z",
+            "periodEndAt": "2025-07-15T05:00:00Z",
+            "rank": 31
+        },
+        {
+            "periodStartAt": "2025-07-15T05:00:00Z",
+            "periodEndAt": "2025-07-15T06:00:00Z",
+            "rank": 35
+        },
+        {
+            "periodStartAt": "2025-07-15T06:00:00Z",
+            "periodEndAt": "2025-07-15T07:00:00Z",
+            "rank": 37
+        },
+        {
+            "periodStartAt": "2025-07-15T07:00:00Z",
+            "periodEndAt": "2025-07-15T08:00:00Z",
+            "rank": 27
+        },
+        {
+            "periodStartAt": "2025-07-15T08:00:00Z",
+            "periodEndAt": "2025-07-15T09:00:00Z",
+            "rank": 7
+        },
+        {
+            "periodStartAt": "2025-07-15T09:00:00Z",
+            "periodEndAt": "2025-07-15T10:00:00Z",
+            "rank": 8
+        },
+        {
+            "periodStartAt": "2025-07-15T10:00:00Z",
+            "periodEndAt": "2025-07-15T11:00:00Z",
+            "rank": 2
+        },
+        {
+            "periodStartAt": "2025-07-15T11:00:00Z",
+            "periodEndAt": "2025-07-15T12:00:00Z",
+            "rank": 0
+        },
+        {
+            "periodStartAt": "2025-07-15T12:00:00Z",
+            "periodEndAt": "2025-07-15T13:00:00Z",
+            "rank": 1
+        },
+        {
+            "periodStartAt": "2025-07-15T13:00:00Z",
+            "periodEndAt": "2025-07-15T14:00:00Z",
+            "rank": 3
+        },
+        {
+            "periodStartAt": "2025-07-15T14:00:00Z",
+            "periodEndAt": "2025-07-15T15:00:00Z",
+            "rank": 4
+        },
+        {
+            "periodStartAt": "2025-07-15T15:00:00Z",
+            "periodEndAt": "2025-07-15T16:00:00Z",
+            "rank": 12
+        },
+        {
+            "periodStartAt": "2025-07-15T16:00:00Z",
+            "periodEndAt": "2025-07-15T17:00:00Z",
+            "rank": 20
+        },
+        {
+            "periodStartAt": "2025-07-15T17:00:00Z",
+            "periodEndAt": "2025-07-15T18:00:00Z",
+            "rank": 32
+        },
+        {
+            "periodStartAt": "2025-07-15T18:00:00Z",
+            "periodEndAt": "2025-07-15T19:00:00Z",
+            "rank": 38
+        },
+        {
+            "periodStartAt": "2025-07-15T19:00:00Z",
+            "periodEndAt": "2025-07-15T20:00:00Z",
+            "rank": 42
+        },
+        {
+            "periodStartAt": "2025-07-15T20:00:00Z",
+            "periodEndAt": "2025-07-15T21:00:00Z",
+            "rank": 39
+        },
+        {
+            "periodStartAt": "2025-07-15T21:00:00Z",
+            "periodEndAt": "2025-07-15T22:00:00Z",
+            "rank": 33
+        },
+        {
+            "periodStartAt": "2025-07-15T22:00:00Z",
+            "periodEndAt": "2025-07-15T23:00:00Z",
+            "rank": 29
+        },
+        {
+            "periodStartAt": "2025-07-15T23:00:00Z",
+            "periodEndAt": "2025-07-16T00:00:00Z",
+            "rank": 23
+        },
+        {
+            "periodStartAt": "2025-07-16T00:00:00Z",
+            "periodEndAt": "2025-07-16T01:00:00Z",
+            "rank": 18
+        },
+        {
+            "periodStartAt": "2025-07-16T01:00:00Z",
+            "periodEndAt": "2025-07-16T02:00:00Z",
+            "rank": 14
+        },
+        {
+            "periodStartAt": "2025-07-16T02:00:00Z",
+            "periodEndAt": "2025-07-16T03:00:00Z",
+            "rank": 13
+        },
+        {
+            "periodStartAt": "2025-07-16T03:00:00Z",
+            "periodEndAt": "2025-07-16T04:00:00Z",
+            "rank": 16
+        },
+        {
+            "periodStartAt": "2025-07-16T04:00:00Z",
+            "periodEndAt": "2025-07-16T05:00:00Z",
+            "rank": 24
+        },
+        {
+            "periodStartAt": "2025-07-16T05:00:00Z",
+            "periodEndAt": "2025-07-16T06:00:00Z",
+            "rank": 30
+        },
+        {
+            "periodStartAt": "2025-07-16T06:00:00Z",
+            "periodEndAt": "2025-07-16T07:00:00Z",
+            "rank": 34
+        },
+        {
+            "periodStartAt": "2025-07-16T07:00:00Z",
+            "periodEndAt": "2025-07-16T08:00:00Z",
+            "rank": 28
+        },
+        {
+            "periodStartAt": "2025-07-16T08:00:00Z",
+            "periodEndAt": "2025-07-16T09:00:00Z",
+            "rank": 15
+        },
+        {
+            "periodStartAt": "2025-07-16T09:00:00Z",
+            "periodEndAt": "2025-07-16T10:00:00Z",
+            "rank": 9
+        },
+        {
+            "periodStartAt": "2025-07-16T10:00:00Z",
+            "periodEndAt": "2025-07-16T11:00:00Z",
+            "rank": 17
+        },
+        {
+            "periodStartAt": "2025-07-16T11:00:00Z",
+            "periodEndAt": "2025-07-16T12:00:00Z",
+            "rank": 10
+        },
+        {
+            "periodStartAt": "2025-07-16T12:00:00Z",
+            "periodEndAt": "2025-07-16T13:00:00Z",
+            "rank": 6
+        },
+        {
+            "periodStartAt": "2025-07-16T13:00:00Z",
+            "periodEndAt": "2025-07-16T14:00:00Z",
+            "rank": 5
+        },
+        {
+            "periodStartAt": "2025-07-16T14:00:00Z",
+            "periodEndAt": "2025-07-16T15:00:00Z",
+            "rank": 11
+        },
+        {
+            "periodStartAt": "2025-07-16T15:00:00Z",
+            "periodEndAt": "2025-07-16T16:00:00Z",
+            "rank": 21
+        },
+        {
+            "periodStartAt": "2025-07-16T16:00:00Z",
+            "periodEndAt": "2025-07-16T17:00:00Z",
+            "rank": 36
+        },
+        {
+            "periodStartAt": "2025-07-16T17:00:00Z",
+            "periodEndAt": "2025-07-16T18:00:00Z",
+            "rank": 40
+        },
+        {
+            "periodStartAt": "2025-07-16T18:00:00Z",
+            "periodEndAt": "2025-07-16T19:00:00Z",
+            "rank": 44
+        },
+        {
+            "periodStartAt": "2025-07-16T19:00:00Z",
+            "periodEndAt": "2025-07-16T20:00:00Z",
+            "rank": 45
+        },
+        {
+            "periodStartAt": "2025-07-16T20:00:00Z",
+            "periodEndAt": "2025-07-16T21:00:00Z",
+            "rank": 43
+        },
+        {
+            "periodStartAt": "2025-07-16T21:00:00Z",
+            "periodEndAt": "2025-07-16T22:00:00Z",
+            "rank": 41
+        }
+    ]
+    }
+}
---
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/states/awaiting-state-carbon-intensity-chart.test.js
@@ -0,0 +1,69 @@
+import {render, screen} from '@testing-library/react'
+import AwaitingStateCarbonIntensityChart from "./awaiting-state-carbon-intensity-chart.js";
+import {jest} from '@jest/globals';
+import CarbonIntensityDataMock from "./__mocks__/carbon-intensity-data-mock.js";
+
+describe('carbon aware intensity chart', () => {
+    let _fetch;
+    function mockFetch(status, data) {
+        _fetch = global.fetch;
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                status,
+                json: () => Promise.resolve(data),
+            })
+        );
+    }
+
+    afterEach(() => {
+        global.fetch = _fetch;
+    });
+
+    it('Does not render if 404 from the server and scheduled in the past', async () => {
+        mockFetch(404, null)
+        const awaitingState = {
+            "@class": "CarbonAwareAwaitingState",
+            "state": "AWAITING",
+            "from": "2025-07-11T10:00",
+            "to": "2025-07-11T13:00"
+        };
+        const job = {
+            jobHistory: [awaitingState, {
+                "state": "SCHEDULED",
+                "reason": "Good to go!",
+                "scheduledAt": "2025-07-11T10:00"
+            }]
+        };
+
+        render(<AwaitingStateCarbonIntensityChart job={job} jobState={awaitingState}/>)
+
+        const noDataMessage = await screen.findByText(/No forecast available anymore for this date - it has been purged./i);
+        expect(noDataMessage).toBeInTheDocument()
+    })
+
+    it('Time blocks start at midnight and render the first available with intensity data in the title', async () => {
+        mockFetch(200, CarbonIntensityDataMock())
+        const awaitingState = {
+            "@class": "CarbonAwareAwaitingState",
+            "state": "AWAITING",
+            "from": "2025-07-11T10:00",
+            "to": "2025-07-11T13:00"
+        };
+        const job = {
+            jobHistory: [awaitingState, {
+                "state": "SCHEDULED",
+                "reason": "Good to go!"
+            }]
+        };
+
+        render(<AwaitingStateCarbonIntensityChart job={job} jobState={awaitingState}/>)
+
+        const time0000 = await screen.findByTitle("Time 00:00 — no data");
+        const time0100 = await screen.findByTitle("Time 01:00 — no data");
+        const time0200 = await screen.findByTitle("Time 02:00 — Intensity: 25 (0 = best)");
+        expect(time0000).toBeInTheDocument()
+        expect(time0100).toBeInTheDocument()
+        expect(time0200).toBeInTheDocument()
+    });
+
+});
