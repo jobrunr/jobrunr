ae747f0423a6744cbba3b6a2301cc0af35ad7172
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-09-09 09:55:45 +0200

    Add WHY comment for AsyncJobValidationRecorder

--- framework-support/jobrunr-quarkus-extension/quarkus-jobrunr/src/main/java/org/jobrunr/scheduling/AsyncJobValidationRecorder.java
+++ framework-support/jobrunr-quarkus-extension/quarkus-jobrunr/src/main/java/org/jobrunr/scheduling/AsyncJobValidationRecorder.java
@@ -2,6 +2,8 @@ package org.jobrunr.scheduling;
 
 import io.quarkus.runtime.annotations.Recorder;
 
+// why: Quarkus expects to inject a method parameter whose type is annoated with @Recorder for @Record build steps
+// Logic is implemented in AsyncJobPostProcessor
 @Recorder
 public class AsyncJobValidationRecorder {
 }
