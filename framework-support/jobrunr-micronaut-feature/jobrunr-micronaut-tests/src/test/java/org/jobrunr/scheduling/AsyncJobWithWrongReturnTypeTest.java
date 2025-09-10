6d1b4cdedcced4d62b53e2864b5e2694e3860e12
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2025-09-10 13:22:48 +0200

    Run async job validation on startup

--- framework-support/jobrunr-micronaut-feature/jobrunr-micronaut-tests/src/test/java/org/jobrunr/scheduling/AsyncJobWithWrongReturnTypeTest.java
+++ framework-support/jobrunr-micronaut-feature/jobrunr-micronaut-tests/src/test/java/org/jobrunr/scheduling/AsyncJobWithWrongReturnTypeTest.java
@@ -12,12 +12,8 @@ public class AsyncJobWithWrongReturnTypeTest {
 
     @Test
     void classAnnotatedWithAsyncJobAnnotationThrowsExceptionIfMethodReturnsSomething() {
-        try (var context = ApplicationContext.run(PropertySource.of("test", Map.of("test.asyncjob.wrongtype.enabled", "true")))) {
-            assertThatCode(() -> {
-                context.getBean(AsyncJobTestServiceWithWrongReturnType.class);
-            })
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage("An @AsyncJob cannot have a return value. int testMethodAsAsyncJobWithSomeReturnType() is defined as an @AsyncJob but has a return value.");
-        }
+        assertThatCode(() -> ApplicationContext.run(PropertySource.of("test", Map.of("test.asyncjob.wrongtype.enabled", "true"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("An @AsyncJob cannot have a return value. org.jobrunr.scheduling.$AsyncJobTestServiceWithWrongReturnType$Definition$Intercepted$Definition@runAsyncJobWithReturnType is defined as an @AsyncJob but has a return value.");
     }
 }
