154d887bd6319f4334175169cb7c777ab665323e
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2025-10-08 10:58:57 +0200

    Revert LoadingIndicator

--- core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/jobs-table.js
+++ core/src/main/resources/org/jobrunr/dashboard/frontend/src/components/jobs/jobs-table.js
@@ -13,6 +13,7 @@ import {SwitchableTimeFormatter, SwitchableTimeRangeFormatter} from "../utils/ti
 import Tooltip from '@mui/material/Tooltip';
 import {EnergySavingsLeaf} from "@mui/icons-material";
 import {getJobMostRecentState, getJobPreviousState, isCarbonAwaitingState} from "../utils/job-utils";
+import LoadingIndicator from "../LoadingIndicator.js";
 
 const IdColumn = styled(TableCell)`
     width: 20%;
@@ -63,74 +64,78 @@ const JobsTable = ({jobPage, jobState, isLoading}) => {
     const isInAwaitingViewAndJobIsCarbonAware = (job) => jobState === "AWAITING" && isCarbonAwaitingState(getJobMostRecentState(job));
     const isInScheduledViewAndJobIsCarbonAware = (job) => jobState === "SCHEDULED" && isCarbonAwaitingState(getJobPreviousState(job));
 
-    return jobPage.items < 1
-        ? <ItemsNotFound id="no-jobs-found-message">No jobs found</ItemsNotFound>
-        : <>
-            <TableContainer>
-                <Table id="jobs-table" style={{width: "100%"}} aria-label="jobs table">
-                    <TableHead>
-                        <TableRow>
-                            <IdColumn>Id</IdColumn>
-                            <TableCell style={{width: '60%'}}>Job details</TableCell>
-                            <TableCell>{column}</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {jobPage.items.map(job => (
-                            <TableRow key={job.id}>
-                                <IdColumn component="th" scope="row">
-                                    <Link to={{
-                                        pathname: `/dashboard/jobs/${job.id}`,
-                                        job: job
-                                    }}>{job.id}</Link>
-                                </IdColumn>
-                                <TableCell>
-                                    {job.labels &&
-                                        <>
-                                            {job.labels.map((label) => <JobLabel key={label} text={label}/>)}
-                                            <span style={{marginRight: '0.5rem'}}></span>
-                                        </>
-                                    }
-                                    <Link to={{
-                                        pathname: `/dashboard/jobs/${job.id}`,
-                                        job: job
-                                    }}>{job.jobName}</Link>
-                                </TableCell>
-                                <TableCell>
-                                    <div style={{display: "flex", alignItems: "center"}}>
-                                        {isInAwaitingViewAndJobIsCarbonAware(job) &&
-                                            <Tooltip title={
-                                                <>
-                                                    This is a Carbon Aware job that will be scheduled <SwitchableTimeRangeFormatter
-                                                    from={new Date(getJobMostRecentState(job).from)}
-                                                    to={new Date(getJobMostRecentState(job).to)}/>
-                                                </>
-                                            }>
-                                                <EnergySavingsLeaf fontSize="small" color="success" style={{marginRight: "4px"}}/>
-                                            </Tooltip>
-                                        }
-                                        {isInScheduledViewAndJobIsCarbonAware(job) &&
-                                            <Tooltip title="This is a Carbon Aware job">
-                                                <EnergySavingsLeaf fontSize="small" color="success" style={{marginRight: "4px"}}/>
-                                            </Tooltip>
-                                        }
-                                        <SwitchableTimeFormatter date={new Date(columnFunction(job))}/>
-                                    </div>
-                                </TableCell>
+    return isLoading
+        ? <LoadingIndicator/>
+        : <>{jobPage.items < 1
+            ? <ItemsNotFound id="no-jobs-found-message">No jobs found</ItemsNotFound>
+            : <>
+                <TableContainer>
+                    <Table id="jobs-table" style={{width: "100%"}} aria-label="jobs table">
+                        <TableHead>
+                            <TableRow>
+                                <IdColumn>Id</IdColumn>
+                                <TableCell style={{width: '60%'}}>Job details</TableCell>
+                                <TableCell>{column}</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                id="jobs-table-pagination"
-                component="div"
-                rowsPerPageOptions={[]}
-                count={jobPage.total}
-                rowsPerPage={jobPage.limit}
-                page={jobPage.currentPage}
-                onPageChange={handleChangePage}
-            />
+                        </TableHead>
+                        <TableBody>
+                            {jobPage.items.map(job => (
+                                <TableRow key={job.id}>
+                                    <IdColumn component="th" scope="row">
+                                        <Link to={{
+                                            pathname: `/dashboard/jobs/${job.id}`,
+                                            job: job
+                                        }}>{job.id}</Link>
+                                    </IdColumn>
+                                    <TableCell>
+                                        {job.labels &&
+                                            <>
+                                                {job.labels.map((label) => <JobLabel key={label} text={label}/>)}
+                                                <span style={{marginRight: '0.5rem'}}></span>
+                                            </>
+                                        }
+                                        <Link to={{
+                                            pathname: `/dashboard/jobs/${job.id}`,
+                                            job: job
+                                        }}>{job.jobName}</Link>
+                                    </TableCell>
+                                    <TableCell>
+                                        <div style={{display: "flex", alignItems: "center"}}>
+                                            {isInAwaitingViewAndJobIsCarbonAware(job) &&
+                                                <Tooltip title={
+                                                    <>
+                                                        This is a Carbon Aware job that will be scheduled <SwitchableTimeRangeFormatter
+                                                        from={new Date(getJobMostRecentState(job).from)}
+                                                        to={new Date(getJobMostRecentState(job).to)}/>
+                                                    </>
+                                                }>
+                                                    <EnergySavingsLeaf fontSize="small" color="success" style={{marginRight: "4px"}}/>
+                                                </Tooltip>
+                                            }
+                                            {isInScheduledViewAndJobIsCarbonAware(job) &&
+                                                <Tooltip title="This is a Carbon Aware job">
+                                                    <EnergySavingsLeaf fontSize="small" color="success" style={{marginRight: "4px"}}/>
+                                                </Tooltip>
+                                            }
+                                            <SwitchableTimeFormatter date={new Date(columnFunction(job))}/>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    id="jobs-table-pagination"
+                    component="div"
+                    rowsPerPageOptions={[]}
+                    count={jobPage.total}
+                    rowsPerPage={jobPage.limit}
+                    page={jobPage.currentPage}
+                    onPageChange={handleChangePage}
+                />
+            </>
+        }
         </>
 }
 
