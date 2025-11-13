db8d8b74b8e1a916822929d327307e10658d1486
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-11-13 14:27:46 +0100

    fix build

--- core/src/test/java17/org/jobrunr/utils/mapper/jackson/Jackson3JsonMapperTest.java
+++ core/src/test/java17/org/jobrunr/utils/mapper/jackson/Jackson3JsonMapperTest.java
@@ -2,9 +2,6 @@ package org.jobrunr.utils.mapper.jackson;
 
 import org.jobrunr.utils.mapper.AbstractJsonMapperTest;
 import org.jobrunr.utils.mapper.JsonMapper;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class Jackson3JsonMapperTest extends AbstractJsonMapperTest {
 
@@ -13,8 +10,4 @@ class Jackson3JsonMapperTest extends AbstractJsonMapperTest {
         return new Jackson3JsonMapper();
     }
 
-    @Test
-    void expectToFailToMakeSureJava17TestsRunInCI() {
-        assertThat(true).isFalse();
-    }
 }
