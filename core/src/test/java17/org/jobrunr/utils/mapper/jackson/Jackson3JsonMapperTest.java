f19cca32744a41384d4f4ad9be631673051b5d1a
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-11-13 14:12:30 +0100

    make sure test17 runs on the CI

--- core/src/test/java17/org/jobrunr/utils/mapper/jackson/Jackson3JsonMapperTest.java
+++ core/src/test/java17/org/jobrunr/utils/mapper/jackson/Jackson3JsonMapperTest.java
@@ -2,11 +2,19 @@ package org.jobrunr.utils.mapper.jackson;
 
 import org.jobrunr.utils.mapper.AbstractJsonMapperTest;
 import org.jobrunr.utils.mapper.JsonMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Jackson3JsonMapperTest extends AbstractJsonMapperTest {
 
     @Override
     public JsonMapper newJsonMapper() {
         return new Jackson3JsonMapper();
+    }
+
+    @Test
+    void expectToFailToMakeSureJava17TestsRunInCI() {
+        assertThat(true).isFalse();
     }
 }
