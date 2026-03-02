02148b6ee43b05ec9fe50d111290cabc37836de2
rdehuyss <ronald.dehuysser@gmail.com>
2026-03-02 17:28:26 +0100

    Update dependencies and fix sonarqube feedback

--- core/src/main/java/org/jobrunr/utils/StringUtils.java
+++ core/src/main/java/org/jobrunr/utils/StringUtils.java
@@ -109,7 +109,7 @@ public class StringUtils {
 
     public static String urlEncode(String string) {
         try {
-            return URLEncoder.encode(string, UTF_8.toString());
+            return URLEncoder.encode(string, UTF_8.name());
         } catch (Exception e) {
             return string;
         }
