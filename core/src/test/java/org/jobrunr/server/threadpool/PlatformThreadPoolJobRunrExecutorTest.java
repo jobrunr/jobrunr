a28d57afee247114664bebd2538fa8654364e33e
rdehuyss <ronald.dehuysser@gmail.com>
2026-05-29 14:43:54 +0200

    Fix build

--- core/src/test/java/org/jobrunr/server/threadpool/PlatformThreadPoolJobRunrExecutorTest.java
+++ core/src/test/java/org/jobrunr/server/threadpool/PlatformThreadPoolJobRunrExecutorTest.java
@@ -19,7 +19,7 @@ class PlatformThreadPoolJobRunrExecutorTest {
 
     @BeforeEach
     void setUp() {
-        platformThreadPoolJobRunrExecutor = new PlatformThreadPoolJobRunrExecutor(8);
+        platformThreadPoolJobRunrExecutor = new PlatformThreadPoolJobRunrExecutor(2);
     }
 
     @AfterEach
@@ -45,16 +45,37 @@ class PlatformThreadPoolJobRunrExecutorTest {
         platformThreadPoolJobRunrExecutor.scheduleWithFixedDelay(myRunnable2, Duration.ZERO, Duration.ofSeconds(1));
 
         // THEN
-        await().until(() -> myRunnable1.invocationCount == 1);
-        await().until(() -> myRunnable2.invocationCount == 1);
+        await().until(() -> myRunnable1.invocationCount == 1 && myRunnable2.invocationCount == 1);
 
         // WHEN
         platformThreadPoolJobRunrExecutor.cancelScheduledFuturesOfType(MyRunnable.class);
 
         // THEN
-        await().during(3, TimeUnit.SECONDS).until(() -> myRunnable1.invocationCount == 1);
-        await().during(3, TimeUnit.SECONDS).until(() -> myRunnable2.invocationCount == 1);
-        assertThat(platformThreadPoolJobRunrExecutor).hasExecutorWithCorePoolSize(6);
+        await().during(3, TimeUnit.SECONDS).until(() -> myRunnable1.invocationCount == 1 && myRunnable2.invocationCount == 1);
+        assertThat(platformThreadPoolJobRunrExecutor).hasExecutorWithCorePoolSize(2);
+    }
+
+    @Test
+    void testPlatformThreadPoolJobRunrExecutorCanCancelTasksAndReducePoolSize() {
+        // GIVEN
+        MyRunnable myRunnable1 = new MyRunnable(1);
+        MyRunnable myRunnable2 = new MyRunnable(2);
+
+        // WHEN
+        platformThreadPoolJobRunrExecutor.increasePoolSize(2);
+        platformThreadPoolJobRunrExecutor.scheduleWithFixedDelay(myRunnable1, Duration.ZERO, Duration.ofSeconds(1));
+        platformThreadPoolJobRunrExecutor.scheduleWithFixedDelay(myRunnable2, Duration.ZERO, Duration.ofSeconds(1));
+
+        // THEN
+        assertThat(platformThreadPoolJobRunrExecutor).hasExecutorWithCorePoolSize(4);
+        await().until(() -> myRunnable1.invocationCount == 1 && myRunnable2.invocationCount == 1);
+
+        // WHEN
+        platformThreadPoolJobRunrExecutor.cancelScheduledFuturesOfType(MyRunnable.class);
+
+        // THEN
+        await().during(3, TimeUnit.SECONDS).until(() -> myRunnable1.invocationCount == 1 && myRunnable2.invocationCount == 1);
+        assertThat(platformThreadPoolJobRunrExecutor).hasExecutorWithCorePoolSize(2);
     }
 
     private static class MyRunnable implements Runnable {
